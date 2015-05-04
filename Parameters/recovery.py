b6248c1015ca4a7446417b2fbd562efcefcce0b6
Jan Medlock <jan.medlock@oregonstate.edu>
2015-05-04 12:48:05 -0700

    Switch recovery from deterministic to exponential.

--- Parameters/recovery.py
+++ Parameters/recovery.py
@@ -1,10 +1,16 @@
+from scipy import stats
+
 from . import rv
 
 
-class recovery_gen(rv.deterministic):
-    'Transition at infectionDuration with probability 1.'
+class recovery_gen(rv.RV):
+    'Exponential waiting time to recovery with rate infectionDuration.'
 
     def __init__(self, parameters, *args, **kwargs):
-        super(recovery_gen, self).__init__('infectionDuration',
-                                           parameters.infectionDuration,
-                                           *args, **kwargs)
+        self.infectionDuration = parameters.infectionDuration
+
+        distn = stats.expon(scale = self.infectionDuration)
+        super(recovery_gen, self)._copyattrs(distn)
+
+    def __repr__(self):
+        return rv.RV.__repr__(self, ('infectionDuration', ))
