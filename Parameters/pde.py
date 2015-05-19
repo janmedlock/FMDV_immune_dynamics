0a3e973b9ccfc7ed9c08fc517d3e84a071484c16
Jan Medlock <jan.medlock@oregonstate.edu>
2015-05-19 01:25:29 -0600

    * Scale out populationSize for better caching.
    * Check if ODE solver was successful.
    
    * Added function to get period of asymptotic solution.
    
    * For endemic equilibrium, return annual points on limit cycle,
      including multiple points for cycles with period >1.

--- Parameters/pde.py
+++ Parameters/pde.py
@@ -30,16 +30,11 @@ def rhs(Y, t, AM, B, forceOfInfection, recoveryRate):
     return dY
 
 
-def solve(tMax, ageMax, ageStep, parameters):
+def solve(tMax, ageMax, ageStep, parameters, Y0 = None):
     matrices = utility.buildMatrices(parameters,
                                      ageMax = ageMax,
                                      ageStep = ageStep)
     (ages, (B_bar, A, M)) = matrices
-    eigenpair = utility.findDominantEigenpair(parameters,
-                                              _matrices = matrices)
-    eigenvector = eigenpair[1][1]
-    N0 = (eigenvector / integrate.trapz(eigenvector, ages)
-          * parameters.populationSize)
 
     AM = A - M
 
@@ -57,19 +52,31 @@ def solve(tMax, ageMax, ageStep, parameters):
         return integrate.trapz(transmissibility * I, ages) * susceptibility
 
     recoveryRate = 1. / parameters.infectionDuration
-    # Everyone under 2 susceptible (except for maternal immunity).
-    S0 = numpy.where(ages < 2., N0, 0.)
-    # initial infections.
-    I0 = 2. * S0 / integrate.trapz(S0, ages)
-    S0 -= I0
-    R0 = N0 - S0 - I0
 
-    Y0 = numpy.hstack((S0, I0, R0))
+    if Y0 is None:
+        eigenpair = utility.findDominantEigenpair(parameters,
+                                                  _matrices = matrices)
+        eigenvector = eigenpair[1][1]
+        N0 = (eigenvector / integrate.trapz(eigenvector, ages)
+              * parameters.populationSize)
+
+        # Everyone under 2 susceptible (except for maternal immunity).
+        S0 = numpy.where(ages < 2., N0, 0.)
+        # initial infections.
+        I0 = 0.01 * S0
+        S0 -= I0
+        R0 = N0 - S0 - I0
+
+        Y0 = numpy.hstack((S0, I0, R0))
 
     t = numpy.linspace(0., tMax, 1001)
 
-    Y = integrate.odeint(rhs, Y0, t,
-                         args = (AM, B, forceOfInfection, recoveryRate))
+    (Y, info) = integrate.odeint(rhs, Y0, t,
+                                 args = (AM, B, forceOfInfection, recoveryRate),
+                                 mxstep = 10000,
+                                 full_output = True)
+    if numpy.any(numpy.diff(info['tcur']) < -1e-16):
+        raise RuntimeError('ODE solver failed!')
 
     (S, I, R) = numpy.hsplit(Y, 3)
 
@@ -81,12 +88,45 @@ def solve(tMax, ageMax, ageStep, parameters):
     return (t, ages, (M, S, I, R))
 
 
-@utility.shelved
-def getEndemicEquilibrium(parameters, tMax = 20.,
-                          ageMax = 20., ageStep = 0.01):
-    (t, ages, (M, S, I, R)) = solve(tMax, ageMax, ageStep, parameters)
+def getPeriod(t, ages, (M, S, I, R), abserr = 1e-3, relerr = 1e-3,
+              periodMax = 3):
+    Y = numpy.hstack((M + S, I, R))
+    for period in range(1, periodMax + 1):
+        i = numpy.argwhere(t <= t[-1] - period)[-1]
+        if (numpy.linalg.norm(Y[i] - Y[-1])
+            <= (abserr + relerr * numpy.linalg.norm(Y[-1]))):
+            return period
+    else:
+        raise ValueError('period not found!')
     
-    return (ages, (M[-1], S[-1], I[-1], R[-1]))
+
+@utility.shelved
+def _getEndemicEquilibrium(parameters, tMax, ageMax, ageStep):
+    (t, ages, (M, S, I, R)) = solve(tMax, ageMax, ageStep, parameters)
+
+    period = getPeriod(t, ages, (M, S, I, R))
+
+    ICs = []
+    for j in range(period):
+        k = numpy.argwhere(t <= t[-1] - j)[-1, 0]
+        ICs.append((M[k], S[k], I[k], R[k]))
+
+    return (ages, ICs)
+
+    
+def getEndemicEquilibrium(parameters, tMax = 200.,
+                          ageMax = 20., ageStep = 0.01):
+    # The PDE solutions simply scale multiplicatively with
+    # populationSize, so factor that out for more efficient caching.
+    populationSize = parameters.populationSize
+    parameters.populationSize = 1.
+    (ages, ICs) = _getEndemicEquilibrium(parameters,
+                                         tMax, ageMax, ageStep)
+    parameters.populationSize = populationSize
+    
+    ICs_ = [[x * populationSize for x in IC] for IC in ICs]
+
+    return (ages, ICs_)
     
 
 if __name__ == '__main__':
