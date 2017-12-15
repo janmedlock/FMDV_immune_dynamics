a3d66112f315c2d93987d2ee60a3136e9373c7ae
Jan Medlock <jan.medlock@oregonstate.edu>
2017-12-15 12:20:41 -0800

    recruitment_rate.py: Split figures.

--- recruitment_rate.py
+++ recruitment_rate.py
@@ -8,6 +8,7 @@ import numpy
 import pandas
 from scipy import integrate
 from scipy import stats
+import seaborn
 
 import herd
 
@@ -27,12 +28,24 @@ def convolution(t, birth_pdf, maternal_immunity_waning):
         return I
 
 
-def gamma(mean, std):
-    return stats.gamma((mean / std) ** 2,
-                       scale = std ** 2 / mean)
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+def xticks_months(ax, t):
+    every = 3 # months
+    xticks = numpy.arange(t[0], t[-1] + every / 12, every / 12)
+    ax.set_xticks(xticks)
+    xticklabels = itertools.cycle(months[::every])
+    xticklabels = [next(xticklabels) for _ in range(len(xticks))]
+    ax.set_xticklabels(xticklabels)
 
 
 if __name__ == '__main__':
+    n_points = 301
+    t_birth = numpy.linspace(0, 1, n_points)
+    t_waning = numpy.linspace(0, 1, n_points)
+    t_susceptible = numpy.linspace(0, 2, n_points)
+
     parameters = herd.parameters.Parameters()
     rvs = herd.RandomVariables(parameters)
 
@@ -40,36 +53,40 @@ if __name__ == '__main__':
     a0 = 4
     _scale = 1
     def birth_pdf(t):
+        '''
+        Remove age dependence.
+        '''
         return numpy.where((t >= 0) & (t < 1),
                            rvs.birth.pdf(t, t0, a0),
                            0) / _scale
     # Scale so that birth_pdf integrates to 1 over [0, 1y].
     _scale, _ = integrate.quad(birth_pdf, 0, 1)
 
-    # maternal_immunity_waning = rvs.maternal_immunity_waning
-    maternal_immunity_waning = gamma(0.35, 0.2)
+    fig, ax = pyplot.subplots(1, 3)
 
-    t = numpy.linspace(0, 2, 301)
-    df = pandas.DataFrame(index = t)
-    df['Birth'] = birth_pdf(t)
-    # df['Waning'] = maternal_immunity_waning.pdf(t)
-    df['Susceptible'] = convolution(t, birth_pdf, maternal_immunity_waning)
-    df.to_csv('recruitment_rate.csv')
+    birth = birth_pdf(t_birth)
+    ax[0].plot(t_birth, birth)
+    ax[0].set_ylabel('Birth')
+    xticks_months(ax[0], t_birth)
 
-    fig, ax = pyplot.subplots()
-    ax.plot(t, df['Birth'], label = 'Birth')
-    # ax.plot(t, df['Waning'], label = 'Waning')
-    ax.plot(t, df['Susceptible'], label = 'Susceptible')
-    ax.set_ylabel('Probability density')
-    every = 3 # months
-    xticks = numpy.arange(t[0], t[-1] + every / 12, every / 12)
-    ax.set_xticks(xticks)
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    xticklabels = itertools.cycle(months[::every])
-    xticklabels = [next(xticklabels) for _ in range(len(xticks))]
-    ax.set_xticklabels(xticklabels)
-    ax.legend()
+    waning = rvs.maternal_immunity_waning.pdf(t_waning)
+    ax[1].plot(t_waning, waning)
+    ax[1].set_ylabel('Maternal-immunity waning')
+    # `\u2009` is a thin space.
+    ax[1].xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:g}\u2009y'))
+
+    susceptible = convolution(t_susceptible,
+                              birth_pdf,
+                              rvs.maternal_immunity_waning)
+    ax[2].plot(t_susceptible, susceptible)
+    ax[2].set_ylabel('Susceptible recruitment')
+    xticks_months(ax[2], t_susceptible)
+
+    for ax_ in ax:
+        ax_.yaxis.set_major_locator(ticker.NullLocator())
+        ax_.set_ylim(bottom = 0)
+    seaborn.despine(fig, top = True, right = True)
+
     fig.tight_layout()
     fig.savefig('recruitment_rate.png', dpi = 300)
     fig.savefig('recruitment_rate.pdf')
