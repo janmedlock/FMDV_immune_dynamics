7cae55b7ef608db17868b6c2e327e8fc1604b94d
Jan Medlock <jan.medlock@oregonstate.edu>
2018-05-28 13:55:11 -0700

    Lock shelf when opening.

--- herd/shelved.py
+++ herd/shelved.py
@@ -1,8 +1,10 @@
-import functools
-import inspect
-import os.path
-import pickle
+from contextlib import contextmanager
+from functools import wraps
+from inspect import getfile
+from os.path import splitext
+from pickle import HIGHEST_PROTOCOL
 import shelve
+from threading import Lock
 
 
 class Shelved:
@@ -10,25 +12,38 @@ class Shelved:
     The cache key is derived from the `getattr(Parameters, p)`
     for `p` in `parameters_to_keep`.'''
     def __init__(self, *parameters_to_keep):
-        self._parameters_to_keep = parameters_to_keep
+        self.parameters_to_keep = parameters_to_keep
+        self.lock = {}
 
     def get_key(self, parameters):
+        '''Build the key string from `parameters`.'''
         clsname = '{}.{}'.format(parameters.__module__,
                                  parameters.__class__.__name__)
         paramreprs = ('{!r}: {!r}'.format(a, getattr(parameters, a))
-                      for a in self._parameters_to_keep)
+                      for a in self.parameters_to_keep)
         return '<{}: {{{}}}>'.format(clsname, ', '.join(paramreprs))
 
     @staticmethod
     def get_cache_file(func):
-        # Put the cache file in the same directory as the caller
-        # and name it 'module.func.db'.
-        root, _ = os.path.splitext(inspect.getfile(func))
+        '''Get the name of the cache file.
+        It is in the same directory as the caller `func`
+        and named it 'module.func.db'.'''
+        root, _ = splitext(getfile(func))
         return '{}.{}'.format(root, func.__name__)
 
+    @contextmanager
+    def get_shelf(self, func):
+        '''Acquire the lock and open the shelf file.'''
+        f = self.get_cache_file(func)
+        print('Acquiring lock.')
+        with self.lock[f], shelve.open(f, protocol=HIGHEST_PROTOCOL) as shelf:
+            print('Lock acquired.')
+            yield shelf
+        print('Lock released.')
+
     def cached_call(self, func, parameters, *args, **kwargs):
-        with shelve.open(self.get_cache_file(func),
-                         protocol=pickle.HIGHEST_PROTOCOL) as shelf:
+        '''Memoized version of `func`.'''
+        with self.get_shelf(func) as shelf:
             key = self.get_key(parameters)
             try:
                 val = shelf[key]
@@ -40,7 +55,9 @@ class Shelved:
         return val
 
     def __call__(self, func):
-        @functools.wraps(func)
+        '''Build the wrapper memoization function.'''
+        self.lock[self.get_cache_file(func)] = Lock()
+        @wraps(func)
         def wrapped(parameters, *args, **kwargs):
             return self.cached_call(func, parameters, *args, **kwargs)
         # Give access to the original function, too.
