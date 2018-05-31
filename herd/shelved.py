d6eca679ad5640b00c25c2f18100809e366d890b
Jan Medlock <jan.medlock@oregonstate.edu>
2018-05-30 19:00:32 -0700

    Use a multiprocessing.Lock() to protect shelve writes.

--- herd/shelved.py
+++ herd/shelved.py
@@ -1,29 +1,18 @@
-from collections import defaultdict
 from functools import wraps
 from inspect import getfile
 from os.path import splitext
 from pickle import HIGHEST_PROTOCOL
 import shelve
-from threading import Lock
+from multiprocessing import Lock
 
 
 class Shelved:
     '''Decorator to memoize results and store to disk.
     The cache key is derived from the `getattr(Parameters, p)`
     for `p` in `parameters_to_keep`.'''
-    class Locks(defaultdict):
-        '''A `collections.defaultdict()` inside another `defaultdict()`.
-        The inner `defaultdict()` returns a `threading.Lock()`.'''
-        class LocksInner(defaultdict):
-            '''A `collections.defaultdict()` that returns a `threading.Lock()`.'''
-            def __init__(self, *args, **kwargs):
-                super().__init__(Lock, *args, **kwargs)
-        def __init__(self, *args, **kwargs):
-            super().__init__(self.LocksInner, *args, **kwargs)
 
     def __init__(self, *parameters_to_keep):
         self.parameters_to_keep = parameters_to_keep
-        self.lock = self.Locks()
 
     def get_key(self, parameters):
         '''Build the key string from `parameters`.'''
@@ -34,6 +23,28 @@ class Shelved:
         return '<{}: {{{}}}>'.format(clsname, ', '.join(paramreprs))
 
     @staticmethod
+    def get_func_name(func):
+        '''Get `func.__module__`.`func.__name__`.'''
+        return '{}.{}()'.format(func.__module__, func.__name__)
+
+    def cached_call(self, cache_file, write_lock,
+                    func, parameters, *args, **kwargs):
+        '''Memoized version of `func`.'''
+        key = self.get_key(parameters)
+        try:
+            with shelve.open(cache_file, protocol=HIGHEST_PROTOCOL) as shelf:
+                val = shelf[key]
+        except (KeyError, ValueError, TypeError):
+            func_name = self.get_func_name(func)
+            print('{} not in {} cache.  Computing...'.format(key, func_name))
+            val = func(parameters, *args, **kwargs)
+            print('\tFinished computing {}.'.format(func_name))
+            with write_lock, \
+                 shelve.open(cache_file, protocol=HIGHEST_PROTOCOL) as shelf:
+                shelf[key] = val
+        return val
+
+    @staticmethod
     def get_cache_file(func):
         '''Get the name of the cache file.
         It is in the same directory as the caller `func`
@@ -41,32 +52,14 @@ class Shelved:
         root, _ = splitext(getfile(func))
         return '{}.{}'.format(root, func.__name__)
 
-    @staticmethod
-    def get_func_name(func):
-        '''Get `func.__module__`.`func.__name__`.'''
-        return '{}.{}()'.format(func.__module__, func.__name__)
-
-    def cached_call(self, func, parameters, *args, **kwargs):
-        '''Memoized version of `func`.'''
-        cache_file = self.get_cache_file(func)
-        with shelve.open(cache_file, protocol=HIGHEST_PROTOCOL) as shelf:
-            key = self.get_key(parameters)
-            with self.lock[cache_file][key]:
-                try:
-                    val = shelf[key]
-                except (KeyError, ValueError, TypeError):
-                    func_name = self.get_func_name(func)
-                    print('{} not in {} cache.  Computing...'.format(key,
-                                                                     func_name))
-                    val = shelf[key] = func(parameters, *args, **kwargs)
-                    print('\tFinished computing {}.'.format(func_name))
-        return val
-
     def __call__(self, func):
         '''Build the wrapper memoization function.'''
+        cache_file = self.get_cache_file(func)
+        write_lock = Lock()
         @wraps(func)
         def wrapped(parameters, *args, **kwargs):
-            return self.cached_call(func, parameters, *args, **kwargs)
+            return self.cached_call(cache_file, write_lock, func,
+                                    parameters, *args, **kwargs)
         # Give access to the original function, too.
         wrapped.func = func
         return wrapped
