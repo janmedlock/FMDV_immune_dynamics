d2650b67d0204a631a5757f94b0a05906f6c595c
Jan Medlock <jan.medlock@oregonstate.edu>
2018-06-11 19:22:16 -0700

    Add option to return eigenvectors.

--- herd/dominant_eigen.py
+++ herd/dominant_eigen.py
@@ -2,7 +2,8 @@ import numpy
 from scipy import sparse
 
 
-def find(A, which='LR', maxiter=100000, *args, **kwargs):
+def find(A, which='LR', return_eigenvector=True, maxiter=100000,
+         *args, **kwargs):
     '''Find the dominant eigenvalue & eigenvector of `A` using
     `scipy.sparse.linalg.eigs()`, which works for both sparse
     and dense matrices.
@@ -11,13 +12,22 @@ def find(A, which='LR', maxiter=100000, *args, **kwargs):
     # The solver just spins with inf/NaN entries.
     # I think this check handles dense & sparse matrices, etc.
     assert numpy.isfinite(A[numpy.nonzero(A)]).all(), 'A has inf/NaN entries.'
-    L, V = sparse.linalg.eigs(A, k=1, which=which, maxiter=maxiter,
-                              *args, **kwargs)
+    result = sparse.linalg.eigs(A, k=1, which=which, maxiter=maxiter,
+                                return_eigenvectors=return_eigenvector,
+                                *args, **kwargs)
+    if return_eigenvector:
+        L, V = result
+    else:
+        L = result
     l0 = numpy.real_if_close(L[0])
     assert numpy.isreal(l0), 'Complex dominant eigenvalue: {}'.format(l0)
-    v0 = V[:, 0]
-    v0 = numpy.real_if_close(v0 / v0.sum())
-    assert all(numpy.isreal(v0)), 'Complex dominant eigenvector: {}'.format(v0)
-    assert all((numpy.real(v0) >= 0) | numpy.isclose(v0, 0)), \
-        'Negative component in the dominant eigenvector: {}'.format(v0)
-    return (l0, v0)
+    if return_eigenvector:
+        v0 = V[:, 0]
+        v0 = numpy.real_if_close(v0 / v0.sum())
+        assert all(numpy.isreal(v0)), \
+            'Complex dominant eigenvector: {}'.format(v0)
+        assert all((numpy.real(v0) >= 0) | numpy.isclose(v0, 0)), \
+            'Negative component in the dominant eigenvector: {}'.format(v0)
+        return (l0, v0)
+    else:
+        return l0
