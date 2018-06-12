dfb33805ad6de9fbd6052521ea4a2c630313d729
Jan Medlock <jan.medlock@oregonstate.edu>
2018-06-11 20:45:23 -0700

    Compute solution using monodromy matrix.

--- herd/eigen.py
+++ herd/eigen.py
@@ -1,5 +1,5 @@
 import numpy
-from scipy import optimize, sparse
+from scipy import integrate, optimize, sparse
 
 from . import birth
 from . import mortality
@@ -7,62 +7,74 @@ from . import dominant_eigen
 from .shelved import Shelved
 
 
-def _build_ages_and_matrices(parameters, agemax=25, agestep=0.01):
-    ages = numpy.arange(0, agemax + agestep / 2, agestep)
+def _build_ages_and_monodromy_args(parameters, agemax=25, agestep=0.01):
+    ages = numpy.arange(0, agemax, agestep)
+    if not numpy.isclose(ages[-1], agemax):
+        ages = numpy.hstack((ages, agemax))
     N = len(ages)
     # Birth
-    # The first row, B_bar[0], is the cumulative birth hazards over a year
+    # The first row, B[0], is the birth hazard
     # times the probability of female birth.
     birthRV = birth.gen(parameters, _scaling=1)
-    # The cumulative hazard is -logsf.
-    cumulative_hazard = -birthRV.logsf(1, parameters.start_time, ages)
-    birth_rate = (parameters.female_probability_at_birth
-                  * cumulative_hazard)
-    B_bar = sparse.lil_matrix((N, N))
-    B_bar[0] = birth_rate
+    def birth_rate(t):
+        return parameters.female_probability_at_birth * birthRV.hazard(t, ages)
+    B = sparse.lil_matrix((N, N))
+    # Establish sparsity pattern.  This row will get updated at each time.
+    B[0] = 1
     # Mortality and aging
-    mortalityRV = mortality.gen(parameters)
-    mortality_rate = mortalityRV.hazard(ages)
+    mortality_rate = mortality.gen(parameters).hazard(ages)
     # No aging out of the last age group.
     aging_rate = numpy.hstack((1 / numpy.diff(ages), 0))
     T = sparse.dia_matrix((N, N))
     T.setdiag(- mortality_rate - aging_rate, 0)
     T.setdiag(aging_rate[: -1], -1)
     # Convert to CSR for fast multiply.
-    B_bar = B_bar.asformat('csr')
+    B = B.asformat('csr')
     T = T.asformat('csr')
-    # Initial guess for eigenvector.
-    v0 = mortalityRV.sf(ages)
-    return (ages, (B_bar, T, v0))
+    args = (parameters.start_time, N, birth_rate, B, T)
+    return (ages, args)
 
 
-def _build_G(birth_scaling, B_bar, T):
-    return birth_scaling * B_bar + T
+def _monodromy_ODEs(psi, t, birth_scaling, N, birth_rate, B, T):
+    Psi = psi.reshape((N, N))
+    B[0] = birth_scaling * birth_rate(t)
+    dPsi_dt = (B + T) @ Psi
+    return dPsi_dt.reshape(-1)
 
 
-def _find_growth_rate(birth_scaling, B_bar, T, v0):
-    G = _build_G(birth_scaling, B_bar, T)
-    r, _ = dominant_eigen.find(G, v0=v0)
-    return r
+def _find_monodromy_matrix(birth_scaling, start_time, N, birth_rate, B, T):
+    Psi0 = numpy.eye(N)
+    psi0 = Psi0.reshape(-1)
+    # Solve over 1 year, the period of the birth rate.
+    t = (start_time, start_time + 1)
+    args = (birth_scaling, N, birth_rate, B, T)
+    psi = integrate.odeint(_monodromy_ODEs, psi0, t, args=args,
+                           mxstep=100000)[-1]
+    Psi = psi.reshape((N, N))
+    return Psi
+
+
+def _find_growth_rate(birth_scaling, *args):
+    Psi = _find_monodromy_matrix(birth_scaling, *args)
+    R = dominant_eigen.find(Psi, which='LM', return_eigenvector=False)
+    return numpy.log(R)
 
 
 @Shelved('birth_seasonal_coefficient_of_variation',
          'female_probability_at_birth')
-def find_birth_scaling(parameters, matrices=None, *args, **kwargs):
+def find_birth_scaling(parameters, *args, **kwargs):
     '''Find the birth scaling that gives population growth rate r = 0.'''
-    if matrices is None:
-        _, matrices = _build_ages_and_matrices(parameters, *args, **kwargs)
-    B_bar, T, v0 = matrices
+    (ages, args) = _build_ages_and_monodromy_args(parameters, *args, **kwargs)
     a = 0
     # We know that at the lower limit a = 0,
     # `_find_growth_rate(0, ...) < 0`,
     # so we need to find an upper limit `b`
     # with `_find_growth_rate(b, ...) >= 0`.
     b = 1
-    while _find_growth_rate(b, B_bar, T, v0) < 0:
+    while _find_growth_rate(b, *args) < 0:
         a = b
         b *= 2
-    return optimize.brentq(_find_growth_rate, a, b, args=(B_bar, T, v0))
+    return optimize.brentq(_find_growth_rate, a, b, args=args)
 
 
 @Shelved('start_time',
@@ -71,10 +83,10 @@ def find_birth_scaling(parameters, matrices=None, *args, **kwargs):
          'female_probability_at_birth')
 def find_stable_age_structure(parameters, *args, **kwargs):
     '''Find the stable age structure.'''
-    ages, matrices = _build_ages_and_matrices(parameters, *args, **kwargs)
-    B_bar, T, v0 = matrices
-    birth_scaling = find_birth_scaling(parameters, matrices=(B_bar, T, v0))
-    G = _build_G(birth_scaling, B_bar, T)
-    r, v = dominant_eigen.find(G, v0=v0)
-    assert numpy.isclose(r, 0), 'Nonzero growth rate.'
+    birth_scaling = find_birth_scaling(parameters, *args, **kwargs)
+    (ages, args) = _build_ages_and_monodromy_args(parameters, *args, **kwargs)
+    Psi = _find_monodromy_matrix(birth_scaling, *args)
+    R, v = dominant_eigen.find(Psi, which='LM', return_eigenvector=True)
+    assert numpy.isclose(numpy.log(R), 0, atol=1e-6), 'Nonzero growth rate.'
+    v /= integrate.trapz(v, ages)
     return (ages, v)
