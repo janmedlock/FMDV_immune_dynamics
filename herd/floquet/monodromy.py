11ce8350a9e5384ad4f920fac1a3f2f8a9a4edd0
Jan Medlock <jan.medlock@oregonstate.edu>
2018-06-24 14:21:18 -0700

    Split solver initialization into several steps.

--- herd/floquet/monodromy.py
+++ herd/floquet/monodromy.py
@@ -25,7 +25,7 @@ So that the cache keys only depend on the relevant parts of
 >>> PhiT = solver.solve(birth_scaling)
 where `parameters` is a `herd.parameters.Parameters()` instance.'''
 
-from numpy import hstack, ones
+import numpy
 from scipy.sparse import lil_matrix
 
 from herd import birth, mortality, parameters
@@ -58,13 +58,30 @@ class Solver:
     '''Solve the monodromy problem.'''
     def __init__(self, solver_parameters, agemax, agestep):
         self.parameters = solver_parameters
-        # Build the objects needed by the solver.
         self.ages = arange(0, agemax, agestep, endpoint=True)
-        n_ages = self.ages.size
         tstep = agestep
         self.t = arange(0, floquet._period, tstep, endpoint=True)
-        # Set up mortality rate.
         mortalityRV = mortality.from_param_values()
+        self._init_crank_nicolson(tstep, mortalityRV)
+        self._init_births(agestep)
+
+    def _init_implicit_euler(self, tstep, mortalityRV):
+        # The implicit Euler method is
+        # (u_i^n - u_{i - 1}^{n - 1}) / dt = - d_i * u_i^n.
+        # This can be written as
+        # u^1 = M @ u^0,
+        M_implicit_euler = lil_matrix((self.ages.size, self.ages.size))
+        # with
+        # M[i, i - 1] = 1 / (1 + dt * d_i),
+        diag = 1 / (1 + tstep * mortalityRV.hazard(self.ages))
+        M_implicit_euler.setdiag(diag[1 : ], -1)
+        # and, to prevent the last age group from aging out of the population,
+        # M[-1, -1] = 1 / (1 + dt * d_{-1}).
+        M_implicit_euler[-1, -1] = diag[-1]
+        # Convert to CSR for fast left multiplication.
+        self._M_implicit_euler = M_implicit_euler.tocsr()
+
+    def _init_crank_nicolson(self, tstep, mortalityRV):
         # The Crank–Nicolson method is
         # (u_i^n - u_{i - 2}^{n - 2}) / 2 / dt
         # = - d_{i - 1} * (u_i^n + u_{i - 2}^{n - 2}) / 2,
@@ -73,38 +90,29 @@ class Solver:
         # (u_1^n - u_0^{n - 1}) / dt = - d_1 * u_1^n.
         # This can be written as
         # u^n = M_2 @ u^{n - 2} + M_1 @ u^{n - 1},
-        M_crank_nicolson_2 = lil_matrix((n_ages, n_ages))
-        M_crank_nicolson_1 = lil_matrix((n_ages, n_ages))
+        M_crank_nicolson_2 = lil_matrix((self.ages.size, self.ages.size))
+        M_crank_nicolson_1 = lil_matrix((self.ages.size, self.ages.size))
         # with
         # M_2[i, i - 2] = (1 - dt * d_{i - 1}) / (1 + dt * d_{i - 1});
-        diag2 = ((1 - tstep * mortalityRV.hazard(self.ages))
-                 / (1 + tstep * mortalityRV.hazard(self.ages)))
-        M_crank_nicolson_2.setdiag(diag2[1 : -1], -2)
+        diag = ((1 - tstep * mortalityRV.hazard(self.ages))
+                / (1 + tstep * mortalityRV.hazard(self.ages)))
+        M_crank_nicolson_2.setdiag(diag[1 : -1], -2)
         # to prevent the last age group from aging out of the population,
         # M_2[-1, -1] = (1 - dt * d_{-1}) / (1 + dt * d_{-1});
-        M_crank_nicolson_2[-1, -1] = diag2[-1]
+        M_crank_nicolson_2[-1, -1] = diag[-1]
         # M_1[1, 0] = 1 / (1 + dt * d_i);
-        diag1 = 1 / (1 + tstep * mortalityRV.hazard(self.ages))
-        M_crank_nicolson_1[1, 0] = diag1[1]
+        diag[:] = 1 / (1 + tstep * mortalityRV.hazard(self.ages))
+        M_crank_nicolson_1[1, 0] = diag[1]
         # and, to prevent the next to last age group from aging out,
         # M_1[-1, -2] = 1 / (1 + dt * d_{-1}).
-        M_crank_nicolson_1[-1, -2] = diag1[-1]
+        M_crank_nicolson_1[-1, -2] = diag[-1]
         # Convert to CSR for fast left multiplication.
-        self.M_crank_nicolson_2 = M_crank_nicolson_2.tocsr()
-        self.M_crank_nicolson_1 = M_crank_nicolson_1.tocsr()
+        self._M_crank_nicolson_2 = M_crank_nicolson_2.tocsr()
+        self._M_crank_nicolson_1 = M_crank_nicolson_1.tocsr()
         # The first time step, n = 1, uses implicit Euler:
-        # (u_i^n - u_{i - 1}^{n - 1}) / dt = - d_i * u_i^n.
-        # This can be written as
-        # u^1 = M @ u^0,
-        M_implicit_euler = lil_matrix((n_ages, n_ages))
-        # with
-        # M[i, i - 1] = 1 / (1 + dt * d_i),
-        M_implicit_euler.setdiag(diag1[1 : ], -1)
-        # and, to prevent the last age group from aging out of the population,
-        # M[-1, -1] = 1 / (1 + dt * d_{-1}).
-        M_implicit_euler[-1, -1] = diag1[-1]
-        # Convert to CSR for fast left multiplication.
-        self.M_implicit_euler = M_implicit_euler.tocsr()
+        self._init_implicit_euler(tstep, mortalityRV)
+
+    def _init_births(self, agestep):
         # The trapezoid rule for the birth integral for i = 0,
         # u_0^n = \sum_j (b_j^n u_j^n + b_{j + 1}^n u_{j + 1}^n) * da / 2.
         # This can be written as
@@ -113,9 +121,10 @@ class Solver:
         # v = da * [0.5, 1, 1, ..., 1, 1, 0.5].
         # Put `female_probability_at_birth` in there, too, for
         # simplicity & efficiency.
-        self.v_trapezoid = (self.parameters.female_probability_at_birth
-                            * agestep
-                            * hstack((0.5, ones(n_ages - 2), 0.5)))
+        self._v_trapezoid = (
+            self.parameters.female_probability_at_birth
+            * agestep
+            * numpy.hstack((0.5, numpy.ones(self.ages.size - 2), 0.5)))
 
     def solve(self, birth_scaling):
         '''Find the monodromy matrix Phi(T), where T is the period.'''
@@ -126,8 +135,8 @@ class Solver:
             _scaling=birth_scaling)
         # Call the core solver.
         return _monodromy.solve(self.ages, self.t,
-                                self.M_crank_nicolson_2,
-                                self.M_crank_nicolson_1,
-                                self.M_implicit_euler,
-                                self.v_trapezoid,
+                                self._M_crank_nicolson_2,
+                                self._M_crank_nicolson_1,
+                                self._M_implicit_euler,
+                                self._v_trapezoid,
                                 birthRV.hazard)
