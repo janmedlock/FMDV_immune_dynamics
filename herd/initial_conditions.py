24d037b3f370a665e7170a66daf09539f3893c12
Jan Medlock <jan.medlock@oregonstate.edu>
2018-07-09 13:12:02 -0700

    Generalize handling of the random variable for initial conditions.

--- herd/initial_conditions.py
+++ herd/initial_conditions.py
@@ -1,5 +1,6 @@
-from numpy import asarray
-from scipy.stats import bernoulli
+import numpy
+import pandas
+from scipy.stats import multinomial
 
 from herd import age_structure, maternal_immunity_waning
 
@@ -12,15 +13,41 @@ class gen:
         self.maternal_immunity_waningRV = maternal_immunity_waning.gen(
             self.parameters)
 
-    def rvs(self, size=1):
-        ages = self.age_structureRV.rvs(size=size)
-        status_ages = {}
+    def _proportion(self, age):
+        status = {}
         # Who has maternal antibodies.
-        prob_M = self.maternal_immunity_waningRV.sf(ages)
-        is_M = asarray(bernoulli.rvs(prob_M), dtype=bool)
-        status_ages['maternal immunity'] = ages[is_M]
-        ages = ages[~is_M]
+        status['maternal immunity'] = self.maternal_immunity_waningRV.sf(age)
         # Figure out who is S and who is C/R.
-        status_ages['rest'] = ages
+        status['rest'] = 1 - status['maternal immunity']
         # Figure out who is E/I.
+        if numpy.isscalar(age):
+            return pandas.Series(status)
+        else:
+            idx = pandas.Index(age, name='age')
+            return pandas.DataFrame(status, index=idx)
+
+    def rvs(self, size=1):
+        # Pick `size` random ages.
+        ages = self.age_structureRV.rvs(size=size)
+        # Determine the status for each age.
+        proportions = self._proportion(ages)
+        status = proportions.columns
+        status_ages = {k: [] for k in status}
+        # `scipy.stats.multinomial.rvs()` can't handle multiple `p`s,
+        # so we need to loop.
+        for (age, row) in proportions.iterrows():
+            # Randomly pick a status.
+            rv = multinomial.rvs(1, row)
+            # `rv` is an array with `1` in the position
+            # picked and `0`s in the remaining positions.
+            # Convert that to the name.
+            s = status[rv == 1][0]
+            # Add this `age` to the status list.
+            status_ages[s].append(age)
         return status_ages
+
+    def pdf(self, age):
+        # `self._proportion(age) * self.age_structureRV.pdf(age)`
+        # but broadcast the multiplication across rows.
+        return self._proportion(age).mul(self.age_structureRV.pdf(age),
+                                         axis='index')
