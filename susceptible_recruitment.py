e83b22dd1406f26677a725812f02416fc91faec8
Jan Medlock <jan.medlock@oregonstate.edu>
2018-07-27 17:54:47 -0700

    Make recruitment graph twice as wide since its time span is twice as wide.

--- susceptible_recruitment.py
+++ susceptible_recruitment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import joblib
-from matplotlib import dates, pyplot, ticker
+from matplotlib import dates, gridspec, pyplot, ticker
 import numpy
 import pandas
 from scipy import integrate
@@ -13,10 +13,31 @@ import herd.birth
 import herd.maternal_immunity_waning
 
 
+class Birth(herd.birth.gen):
+    '''Birth cut off after 1 year.'''
+    def hazard(self, time, time0, age0):
+        return numpy.where(time < 1,
+                           super().hazard(time + time0, age0 + time),
+                           0)
+
+    def logsf(self, time, time0, age0):
+        return numpy.where(time < 1,
+                           super().logsf(time, time0, age0),
+                           super().logsf(1, time0, age0))
+
+    def pdf(self, time, time0, age0):
+        return (self.hazard(time, time0, age0)
+                * self.sf(time, time0, age0))
+
+    def logpdf(self, time, time0, age0):
+        return (numpy.log(self.hazard(time, time0, age0))
+                + self.logsf(time, time0, age0))
+
+
 class SusceptibleRecruitment(herd.rv.RV):
     '''The mixture of birth and maternal-immunity waning.'''
     def __init__(self, parameters, *args, **kwargs):
-        self._birth = herd.birth.gen(parameters)
+        self._birth = Birth(parameters)
         self._maternal_immunity_waning = herd.maternal_immunity_waning.gen(
             parameters)
 
@@ -24,43 +45,46 @@ class SusceptibleRecruitment(herd.rv.RV):
         return (self._birth.pdf(s, t0, a0)
                 * self._maternal_immunity_waning.pdf(t - s))
 
-    def _pdf_single(self, t, t0, a0, maxiter=10000):
-        pdf, _ = integrate.quadrature(self._pdf_integrand, t0, t,
-                                      args=(t, t0, a0),
-                                      maxiter=maxiter)
-        assert pdf >= 0
-        return pdf
-
-    def pdf(self, t, t0, a0):
+    def pdf(self, t, t0, a0, maxiter=10000):
         '''The PDF is
         p_{susceptible}(t, t0, a0)
         = \int_{t0}^t p_{birth}(s, t0, a0) p_{waning}(t - s) ds.'''
-        return numpy.array(joblib.Parallel(n_jobs=-1)(
-            joblib.delayed(self._pdf_single)(t_, t0, a0)
-            for t_ in t))
+        if numpy.isscalar(t):
+            pdf, _ = integrate.quadrature(self._pdf_integrand, t0, t,
+                                          args=(t, t0, a0),
+                                          maxiter=maxiter)
+            assert pdf >= 0
+            return pdf
+        else:
+            return numpy.array(joblib.Parallel(n_jobs=-1)(
+                joblib.delayed(self.pdf)(t_, t0, a0)
+                for t_ in t))
 
     def _cdf_integrand(self, s, t, t0, a0):
         return (self._birth.pdf(s, t0, a0)
                 * self._maternal_immunity_waning.cdf(t - s))
 
-    def _cdf_single(self, t, t0, a0, maxiter=10000):
-        cdf, _ = integrate.quadrature(self._cdf_integrand, t0, t,
-                                      args=(t, t0, a0),
-                                      maxiter=maxiter)
-        assert 0 <= cdf <= 1
-        return cdf
-
-    def cdf(self, t, t0, a0):
+    def cdf(self, t, t0, a0, maxiter=10000):
         '''The CDF is
         P_{susceptible}(t, t0, a0)
         = \int_{t0}^t p_{susceptible}(s, t0, a0) ds
         = \int_{t0}^t p_{birth}(s, t0, a0) P_{waning}(t - s) ds.'''
-        return numpy.array(joblib.Parallel(n_jobs=-1)(
-            joblib.delayed(self._cdf_single)(t_, t0, a0)
-            for t_ in t))
+        if numpy.isscalar(t):
+            cdf, _ = integrate.quadrature(self._cdf_integrand, t0, t,
+                                          args=(t, t0, a0),
+                                          maxiter=maxiter)
+            assert 0 <= cdf <= 1
+            return cdf
+        else:
+            return numpy.array(joblib.Parallel(n_jobs=-1)(
+                joblib.delayed(self.cdf)(t_, t0, a0)
+                for t_ in t))
+
+    def sf(self, t, t0, a0):
+        return 1 - self.cdf(t, t0, a0)
 
     def hazard(self, t, t0, a0):
-        return self.pdf(t, t0, a0) / (1 - self.cdf(t, t0, a0))
+        return self.pdf(t, t0, a0) / self.sf(t, t0, a0)
 
 
 def timestamp2year(ts):
@@ -88,46 +112,52 @@ def MonthLocator(interval=1):
 if __name__ == '__main__':
     # Birth depends on time of year.
     # 1 year of dates, points every day.
-    start = pandas.Timestamp(year=2001, month=1, day=1)
+    start = pandas.Timestamp(year=2001, month=7, day=1)
     end = start + pandas.DateOffset(years=1) - pandas.DateOffset(days=1)
     freq = 'D'
     t_birth = pandas.date_range(start, end, freq=freq)
     # Waning depends only on time since birth, not time of year.
     # 1 year of time intervals, points every day.
-    t_waning = pandas.timedelta_range(0, periods=dates.DAYS_PER_YEAR, freq=freq)
+    t_waning = pandas.timedelta_range(0, periods=dates.DAYS_PER_YEAR,
+                                      freq=freq)
     # Susceptible recruitment depends on time of year.
     # 2 years of dates, points every day.
     end = start + pandas.DateOffset(years=2) - pandas.DateOffset(days=1)
     t_susceptible = pandas.date_range(start, end, freq=freq)
 
     parameters = herd.Parameters()
-    birth = herd.birth.gen(parameters)
+    birth = Birth(parameters)
     maternal_immunity_waning = herd.maternal_immunity_waning.gen(parameters)
     susceptible_recruitment = SusceptibleRecruitment(parameters)
 
     # For births, to remove age dependence.
     a0 = 4  # years.
 
-    hazard = pandas.read_csv('susceptible_recruitment.csv')
-    # hazard = {}
-    # hazard['Birth'] = pandas.Series(
-    #     birth.hazard(timestamp2year(t_birth), a0),
-    #     index=t_birth)
-    # # Convert the index for waning from a `pandas.TimedeltaIndex` to a
-    # # `pandas.DatetimeIndex` like the other 2.
-    # hazard['Maternal-immunity waning'] = pandas.Series(
-    #     maternal_immunity_waning.hazard(timedelta2year(t_waning)),
-    #     index=t_waning + start)
-    # hazard['Susceptible recruitment'] = pandas.Series(
-    #     susceptible_recruitment.hazard(timestamp2year(t_susceptible),
-    #                                    timestamp2year(t_susceptible[0]), a0),
-    #     index=t_susceptible)
-    # hazard = pandas.DataFrame(hazard)
-    # hazard.to_csv('susceptible_recruitment.csv')
+    try:
+        df = pandas.read_csv('susceptible_recruitment.csv',
+                             index_col=0)
+    except FileNotFoundError:
+        df = {}
+        t = timestamp2year(t_birth)
+        df['Birth'] = pandas.Series(birth.hazard(t - t[0], t[0], a0),
+                                    index=t_birth)
+        # Convert the index for waning from a `pandas.TimedeltaIndex` to a
+        # `pandas.DatetimeIndex` like the other 2.
+        df['Maternal-immunity waning'] = pandas.Series(
+            maternal_immunity_waning.hazard(timedelta2year(t_waning)),
+            index=t_waning + start)
+        t = timestamp2year(t_susceptible)
+        df['Susceptible recruitment'] = pandas.Series(
+            susceptible_recruitment.hazard(t - t[0], t[0], a0),
+            index=t_susceptible)
+        df = pandas.DataFrame(df)
+        df.to_csv('susceptible_recruitment.csv')
 
-    fig, ax = pyplot.subplots(1, 3)
+    fig = pyplot.figure()
+    gs = gridspec.GridSpec(1, 3, width_ratios=(1, 1, 2))
+    ax = [fig.add_subplot(x) for x in gs]
 
-    ax[0].plot(t_birth, hazard['Birth'].dropna(),
+    ax[0].plot(t_birth, df['Birth'].dropna(),
                color='C0', clip_on=False)
     ax[0].set_ylabel('Birth')
     # Major ticks every 6 months and minor ticks every month.
@@ -136,7 +166,7 @@ if __name__ == '__main__':
     ax[0].xaxis.set_major_formatter(dates.DateFormatter('%b'))
 
     ax[1].plot(timedelta2year(t_waning),
-               hazard['Maternal-immunity waning'].dropna(),
+               df['Maternal-immunity waning'].dropna(),
                color='C1', clip_on=False)
     ax[1].set_ylabel('Maternal-immunity waning')
     # Major ticks automatic and minor ticks every month.
@@ -144,7 +174,7 @@ if __name__ == '__main__':
     # '\u2009' is thin space.
     ax[1].xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:g}\u2009y'))
 
-    ax[2].plot(t_susceptible, hazard['Susceptible recruitment'].dropna(),
+    ax[2].plot(t_susceptible, df['Susceptible recruitment'].dropna(),
                color='C2', clip_on=False)
     ax[2].set_ylabel('Susceptible recruitment')
     # Major ticks every 6 months and minor ticks every month.
