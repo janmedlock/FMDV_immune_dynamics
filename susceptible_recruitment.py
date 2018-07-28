e396aba2a500284ef0e54873182ffdd881cce8d3
Jan Medlock <jan.medlock@oregonstate.edu>
2018-07-27 18:38:31 -0700

    Plot density and only susceptible recruitment.

--- susceptible_recruitment.py
+++ susceptible_recruitment.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import operator
+
 import joblib
 from matplotlib import dates, gridspec, pyplot, ticker
 import numpy
@@ -110,81 +112,42 @@ def MonthLocator(interval=1):
 
 
 if __name__ == '__main__':
-    # Birth depends on time of year.
-    # 1 year of dates, points every day.
-    start = pandas.Timestamp(year=2001, month=7, day=1)
-    end = start + pandas.DateOffset(years=1) - pandas.DateOffset(days=1)
-    freq = 'D'
-    t_birth = pandas.date_range(start, end, freq=freq)
-    # Waning depends only on time since birth, not time of year.
-    # 1 year of time intervals, points every day.
-    t_waning = pandas.timedelta_range(0, periods=dates.DAYS_PER_YEAR,
-                                      freq=freq)
+    # 'hazard', 'pdf', 'cdf', or 'sf'.
+    stat = operator.attrgetter('pdf')
+
     # Susceptible recruitment depends on time of year.
     # 2 years of dates, points every day.
+    start = pandas.Timestamp(year=2001, month=7, day=1)
     end = start + pandas.DateOffset(years=2) - pandas.DateOffset(days=1)
+    freq = 'D'
     t_susceptible = pandas.date_range(start, end, freq=freq)
 
     parameters = herd.Parameters()
-    birth = Birth(parameters)
-    maternal_immunity_waning = herd.maternal_immunity_waning.gen(parameters)
     susceptible_recruitment = SusceptibleRecruitment(parameters)
 
     # For births, to remove age dependence.
     a0 = 4  # years.
 
     try:
-        df = pandas.read_csv('susceptible_recruitment.csv',
-                             index_col=0)
+        ser = pandas.read_csv('susceptible_recruitment.csv',
+                              index_col=0, header=None, squeeze=True)
     except FileNotFoundError:
-        df = {}
-        t = timestamp2year(t_birth)
-        df['Birth'] = pandas.Series(birth.hazard(t - t[0], t[0], a0),
-                                    index=t_birth)
-        # Convert the index for waning from a `pandas.TimedeltaIndex` to a
-        # `pandas.DatetimeIndex` like the other 2.
-        df['Maternal-immunity waning'] = pandas.Series(
-            maternal_immunity_waning.hazard(timedelta2year(t_waning)),
-            index=t_waning + start)
         t = timestamp2year(t_susceptible)
-        df['Susceptible recruitment'] = pandas.Series(
-            susceptible_recruitment.hazard(t - t[0], t[0], a0),
+        ser = pandas.Series(
+            stat(susceptible_recruitment)(t - t[0], t[0], a0),
             index=t_susceptible)
-        df = pandas.DataFrame(df)
-        df.to_csv('susceptible_recruitment.csv')
+        ser.to_csv('susceptible_recruitment.csv')
 
-    fig = pyplot.figure()
-    gs = gridspec.GridSpec(1, 3, width_ratios=(1, 1, 2))
-    ax = [fig.add_subplot(x) for x in gs]
+    fig, ax = pyplot.subplots()
 
-    ax[0].plot(t_birth, df['Birth'].dropna(),
-               color='C0', clip_on=False)
-    ax[0].set_ylabel('Birth')
+    ax.plot(t_susceptible, ser)
+    ax.set_xlabel('Susceptible recruitment')
+    ax.set_ylabel(r'Density ($\mathrm{y}^{-1}$)')
     # Major ticks every 6 months and minor ticks every month.
-    ax[0].xaxis.set_major_locator(MonthLocator(interval=6))
-    ax[0].xaxis.set_minor_locator(ticker.AutoMinorLocator(6))
-    ax[0].xaxis.set_major_formatter(dates.DateFormatter('%b'))
+    ax.xaxis.set_major_locator(MonthLocator(interval=3))
+    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator(3))
+    ax.xaxis.set_major_formatter(dates.DateFormatter('%b'))
 
-    ax[1].plot(timedelta2year(t_waning),
-               df['Maternal-immunity waning'].dropna(),
-               color='C1', clip_on=False)
-    ax[1].set_ylabel('Maternal-immunity waning')
-    # Major ticks automatic and minor ticks every month.
-    ax[1].xaxis.set_minor_locator(ticker.MultipleLocator(1 / 12))
-    # '\u2009' is thin space.
-    ax[1].xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:g}\u2009y'))
-
-    ax[2].plot(t_susceptible, df['Susceptible recruitment'].dropna(),
-               color='C2', clip_on=False)
-    ax[2].set_ylabel('Susceptible recruitment')
-    # Major ticks every 6 months and minor ticks every month.
-    ax[2].xaxis.set_major_locator(MonthLocator(interval=6))
-    ax[2].xaxis.set_minor_locator(ticker.AutoMinorLocator(6))
-    ax[2].xaxis.set_major_formatter(dates.DateFormatter('%b'))
-
-    for ax_ in ax:
-        ax_.yaxis.set_major_locator(ticker.NullLocator())
-        ax_.set_ylim(bottom=0)
     seaborn.despine(fig, top=True, right=True)
 
     fig.tight_layout()
