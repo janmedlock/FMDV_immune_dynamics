56a2ab791434000fe26e23ae4f96955f73cbef6e
Jan Medlock <jan.medlock@oregonstate.edu>
2018-09-24 12:01:58 -0700

    Update for Simon's new posterior samples.

--- herd/samples.py
+++ herd/samples.py
@@ -5,36 +5,45 @@ import pandas
 
 _path = os.path.join(os.path.dirname(__file__), 'data')
 
-_samples = {}
-for SAT in (1, 2, 3):
-    _filename = os.path.join(
-        _path, 'SAT{}_SEIRModel_PosteriorSamples.txt'.format(SAT))
-    _samples[SAT] = pandas.read_csv(_filename,
-                                    delim_whitespace=True,
-                                    header=None,
-                                    names=('progression_shape',
-                                           'progression_mean',
-                                           'recovery_shape',
-                                           'recovery_mean',
-                                           'transmission_rate'))
-    # Convert from days to years.
-    _samples[SAT]['progression_mean'] /= 365
-    _samples[SAT]['recovery_mean'] /= 365
-    # Convert from per day to per year.
-    _samples[SAT]['transmission_rate'] *= 365
-
-_filename = os.path.join(_path, 'AbDuration_PosteriorSamples.txt')
-# `None` causes an error in `pandas.concat()`.
-_samples[0] = pandas.read_csv(_filename,
-                              delim_whitespace=True,
-                              header=None,
-                              names=('maternal_immunity_duration_shape',
-                                     'maternal_immunity_duration_mean'))
-
-samples = pandas.concat(_samples, axis=1, names=['SAT'])
-samples.columns.set_names('parameter', level=1, inplace=True)
-# Convert SAT `0` to `None`.
-_l = list(samples.columns.levels[0])
-_l[_l.index(0)] = None
-samples.columns.set_levels(_l, level=0, inplace=True)
+_filename = os.path.join(_path, 'MCMCSamples_SEIRModel.txt')
+samples = pandas.read_csv(_filename,
+                          delim_whitespace=True,
+                          header=None)
 samples.index.name = 'sample'
+_params = pandas.Index(['progression_shape',
+                        'progression_mean',
+                        'recovery_shape',
+                        'recovery_mean',
+                        'transmission_rate'],
+                       name='parameter')
+_SATs = pandas.RangeIndex(1, 3 + 1,
+                          name='SAT')
+_names = [_params.name, _SATs.name]  # Stupid pandas doesn't propagate names.
+# Put 'parameter' first for convenience below.
+# The order will be flipped below so 'SAT' is first.
+samples.columns = pandas.MultiIndex.from_product([_params, _SATs],
+                                                 names=_names)
+
+# Convert from days to years.
+samples['progression_mean'] /= 365
+samples['recovery_mean'] /= 365
+# Convert from per day to per year.
+samples['transmission_rate'] *= 365
+
+# The durations of maternal antibodies, which are the same for each SAT.
+_filename_ma = os.path.join(_path, 'AbDuration_PosteriorSamples.txt')
+_samples_ma = pandas.read_csv(_filename_ma,
+                              delim_whitespace=True,
+                              header=None)
+_samples_ma.index.name = samples.index.name
+_samples_ma.columns = pandas.Index(['maternal_immunity_duration_shape',
+                                    'maternal_immunity_duration_mean'],
+                                   name='parameter')
+# Add the maternal-antibody parameters to each SAT.
+for _param in _samples_ma.columns:
+    for _SAT in _SATs:
+        samples[(_param, _SAT)] = _samples_ma[_param]
+
+# Flip order of column MultiIndex levels so 'SAT' is first.
+samples.columns = samples.columns.reorder_levels(['SAT', 'parameter'])
+samples.sort_index(axis='columns', inplace=True)
