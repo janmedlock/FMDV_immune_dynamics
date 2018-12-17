fa0547180e4abff6d19e5232e901ba1966d15796
Jan Medlock <jan.medlock@oregonstate.edu>
2018-12-17 14:54:22 -0800

    Move Event()s themselves into herd/event.py.  Move Events() into herd/buffalo.py.

--- herd/buffalo.py
+++ herd/buffalo.py
@@ -1,170 +1,22 @@
-import numpy
-from scipy import stats
-
-from herd.events import Event, Events
+from herd import event
 
 
-def Sex(buffalo):
-    '''The buffalo having its sex determined.'''
-    if buffalo.herd.rvs.female.rvs() == 1:
-        return 'female'
-    else:
-        return 'male'
+class Events(dict):
+    '''Container to hold all events that can happen to a buffalo.'''
+    # Consider storing the events in a data type that's more
+    # efficient to find the minimum time.
+    def __init__(self, buffalo, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.buffalo = buffalo
 
+    def add(self, event_):
+        self[event_.name] = event_
 
-class Mortality(Event):
-    '''The buffalo dying.'''
-    def is_valid(self):
-        return True  # All buffalo can die.
+    def remove(self, event_):
+        del self[event_.name]
 
-    def do(self):
-        self.buffalo.herd.remove(self.buffalo)
-
-    def sample_time(self):
-        # Use resampling to get a mortality time > current time.
-        while True:
-            time = (self.buffalo.birth_date
-                    + self.buffalo.herd.rvs.mortality.rvs())
-            if time > self.buffalo.herd.time:
-                return time
-
-
-class Birth(Event):
-    '''The buffalo giving birth.'''
-    # Which classes give mom antibodies that she passes on.
-    has_antibodies = ('chronic', 'recovered')
-
-    def is_valid(self):
-        return self.buffalo.sex == 'female'
-
-    def do(self):
-        if self.buffalo.immune_status in self.has_antibodies:
-            calf_status = 'maternal immunity'
-        else:
-            calf_status = 'susceptible'
-        self.buffalo.herd.append(Buffalo(self.buffalo.herd, calf_status))
-        # Update to time of the next birth.
-        self.time = self.sample_time()
-
-    def sample_time(self):
-        return (self.buffalo.herd.time
-                + self.buffalo.herd.rvs.birth.rvs(self.buffalo.herd.time,
-                                                  self.buffalo.age))
-
-
-class MaternalImmunityWaning(Event):
-    '''The buffalo losing maternal immunity.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'maternal immunity'
-
-    def do(self):
-        self.buffalo.change_immune_status_to('susceptible')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(Infection(self.buffalo))
-
-    def sample_time(self):
-        # Use resampling to get a waning time > current time.
-        while True:
-            time = (self.buffalo.birth_date
-                    + self.buffalo.herd.rvs.maternal_immunity_waning.rvs())
-            if time >= self.buffalo.herd.time:
-                return time
-
-
-class Infection(Event):
-    '''The buffalo becoming infected.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'susceptible'
-
-    def do(self):
-        self.buffalo.change_immune_status_to('exposed')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(Progression(self.buffalo))
-
-    def sample_time(self):
-        force_of_infection = (
-            (self.buffalo.herd.rvs.transmission_rate
-             * self.buffalo.herd.number_infectious)
-            + (self.buffalo.herd.rvs.chronic_transmission_rate
-               * self.buffalo.herd.number_chronic))
-        # scale = 1 / force_of_infection
-        # Handle division by 0.
-        scale = numpy.ma.filled(
-            numpy.ma.divide(1, force_of_infection),
-            numpy.inf)
-        return (self.buffalo.herd.time
-                + stats.expon.rvs(scale=scale))
-
-
-class Progression(Event):
-    '''The buffalo becoming infectious.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'exposed'
-
-    def do(self):
-        self.buffalo.change_immune_status_to('infectious')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(Recovery(self.buffalo))
-
-    def sample_time(self):
-        return (self.buffalo.herd.time
-                + self.buffalo.herd.rvs.progression.rvs())
-
-
-class Recovery(Event):
-    '''The buffalo recovering from acute infection.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'infectious'
-
-    def do(self):
-        if self.buffalo.herd.rvs.probability_chronic.rvs() == 1:
-            self.do_chronic_progression()
-        else:
-            self.do_recovery()
-
-    def do_recovery(self):
-        self.buffalo.change_immune_status_to('recovered')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(ImmunityWaning(self.buffalo))
-
-    def do_chronic_progression(self):
-        self.buffalo.change_immune_status_to('chronic')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(ChronicRecovery(self.buffalo))
-
-    def sample_time(self):
-        return (self.buffalo.herd.time
-                + self.buffalo.herd.rvs.recovery.rvs())
-
-
-class ChronicRecovery(Event):
-    '''The buffalo recovering from chronically infected.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'chronic'
-
-    def do(self):
-        self.buffalo.change_immune_status_to('recovered')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(ImmunityWaning(self.buffalo))
-
-    def sample_time(self):
-        return (self.buffalo.herd.time
-                + self.buffalo.herd.rvs.chronic_recovery.rvs())
-
-
-class ImmunityWaning(Event):
-    '''The buffalo losing its acquired immunity.'''
-    def is_valid(self):
-        return self.buffalo.immune_status == 'recovered'
-
-    def do(self):
-        self.buffalo.change_immune_status_to('susceptible')
-        self.buffalo.events.remove(self)
-        self.buffalo.events.add(Infection(self.buffalo))
-
-    def sample_time(self):
-        return (self.buffalo.herd.time
-                + self.buffalo.herd.rvs.immunity_waning.rvs())
+    def get_next(self):
+        return min(self.values())
 
 
 class Buffalo:
@@ -173,7 +25,7 @@ class Buffalo:
         self.herd = herd
         self.immune_status = immune_status
         self.birth_date = self.herd.time - age
-        self.sex = Sex(self)
+        self.sex = event.Sex(self)
         self.identifier = next(self.herd.identifiers)
         if self.herd.debug:
             if age == 0:
@@ -188,32 +40,32 @@ class Buffalo:
                     age,
                     immune_status))
         self.events = Events(self)
-        # Add all valid `Event()` subclasses.
-        # for klass in Event.__subclasses__():
+        # Add all valid `_Event()` subclasses.
+        # for klass in event._Event.__subclasses__():
         #     try:
-        #         event = klass(self)
+        #         self.events.add(klass(self))
         #     except AssertionError:
         #         pass
-        #     else:
-        #         self.events.add(event)
-        self.events.add(Mortality(self))
+        self.events.add(event.Mortality(self))
         if self.sex == 'female':
-            self.events.add(Birth(self))
+            self.events.add(event.Birth(self))
         if self.immune_status == 'maternal immunity':
-            self.events.add(MaternalImmunityWaning(self))
+            self.events.add(event.MaternalImmunityWaning(self))
         elif self.immune_status == 'susceptible':
             # When building a new herd, the infection time
             # won't be correct because
             # `self.herd.number_infectious` won't be finalized.
-            self.events.add(Infection(self))
+            # This gets fixed by `herd.Herd()`
+            # calling `update_infection()` after the herd is built.
+            self.events.add(event.Infection(self))
         elif self.immune_status == 'exposed':
-            self.events.add(Progression(self))
+            self.events.add(event.Progression(self))
         elif self.immune_status == 'infectious':
-            self.events.add(Recovery(self))
+            self.events.add(event.Recovery(self))
         elif self.immune_status == 'chronic':
-            self.events.add(ChronicRecovery(self))
+            self.events.add(event.ChronicRecovery(self))
         elif self.immune_status == 'recovered':
-            self.events.add(ImmunityWaning(self))
+            self.events.add(event.ImmunityWaning(self))
         else:
             raise ValueError('Unknown immune_status = {}!'.format(
                 self.immune_status))
@@ -228,11 +80,9 @@ class Buffalo:
         self.herd.immune_status_append(self)
 
     def update_infection(self):
-        event = self.events['Infection']
-        assert event.is_valid()
-        event.time = event.sample_time()
+        infection = self.events['Infection']
+        assert infection.is_valid()
+        infection.time = infection.sample_time()
 
     def get_next_event(self):
-        # Consider storing the events in a data type that's more
-        # efficient to find the minimum.
         return self.events.get_next()
