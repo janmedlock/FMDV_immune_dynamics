5cb2b46b5b165e028b7f4a261c74bbdf43dd54aa
Jan Medlock <jan.medlock@oregonstate.edu>
2019-01-15 12:45:31 -0800

    Save memory by only loading parts of the HDF files.

--- h5.py
+++ h5.py
@@ -7,8 +7,38 @@ import numpy
 import pandas
 
 
-def load(filename, key='df'):
-    return pandas.read_hdf(filename, key)
+class HDFStore(pandas.HDFStore):
+    '''pandas.HDFStore() with fixed key.'''
+    def __init__(self, path, key='df', *args, **kwds):
+        super().__init__(path, *args, **kwds)
+        self.key = key
+        self._index = None
+        self._columns = None
+
+    def get(self):
+        return super().get(self.key)
+
+    def select(self, *args, **kwds):
+        return super().select(self.key, *args, **kwds)
+
+    def put(self, *args, **kwds):
+        return super().put(self.key, *args, **kwds)
+
+    @property
+    def index(self):
+        if self._index is None:
+            self._index = self.select(columns=[]).index
+        return self._index
+
+    @property
+    def columns(self):
+        if self._columns is None:
+            self._columns = self.select(stop=0).columns
+        return self._columns
+
+
+def load(filename, key='df', **kwds):
+    return pandas.read_hdf(filename, key, **kwds)
 
 
 def dump(df, filename, key='df', mode='w', format='table'):
--- plot_start_times_SATs.py
+++ plot_start_times_SATs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+import itertools
 import os.path
 
 from matplotlib import pyplot
+import matplotlib.collections
 import numpy
 import pandas
 import seaborn
@@ -13,41 +15,49 @@ import h5
 t_name = 'time (y)'
 
 
-def get_data(chronic=False):
+def get_filename(chronic=False):
     filename = 'run_start_times_SATs'
     if chronic:
         filename += '_chronic'
     filename += '.h5'
-    data = h5.load(filename)
+    return filename
+
+
+def get_downsampled(chronic=False):
+    filename = get_filename(chronic=chronic)
     base, ext = os.path.splitext(filename)
     filename_ds = base + '_downsampled' + ext
     try:
         data_ds = h5.load(filename_ds)
     except FileNotFoundError:
-        data_ds = downsample(data)
+        data_ds = downsample(filename)
         h5.dump(data_ds, filename_ds)
-    return (data, data_ds)
+    return data_ds
 
 
-def downsample(data):
-    not_t_names = [n for n in data.index.names if n != t_name]
+def downsample(filename):
+    store = h5.HDFStore(filename, mode='r')
+    not_t_names = [n for n in store.index.names if n != t_name]
     # Daily timesteps.
-    t = data.index.get_level_values(t_name).unique()
+    t = store.index.get_level_values(t_name)
     t = numpy.arange(t.min(), t.max(), 1 / 365)
-    not_t = [data.index.get_level_values(n).unique()
+    not_t = [store.index.get_level_values(n).unique()
              for n in not_t_names]
     midx = pandas.MultiIndex.from_product(not_t + [t],
-                                          names=data.index.names)
-    data_ds = pandas.DataFrame(index=midx, columns=data.columns)
+                                          names=store.index.names)
+    data_ds = pandas.DataFrame(index=midx,
+                               columns=store.columns)
     # Loop over each run.
-    for k, x in data.groupby(level=not_t_names):
-        x = x.copy()
+    for ix in itertools.product(*not_t):
+        where = ' & '.join(f'{k}={v}' for k, v in zip(not_t_names, ix))
+        print(where)
+        data = store.select(where=where)
         # Only keep time index.
-        x.index = x.index.get_level_values(t_name)
+        data.index = data.index.get_level_values(t_name)
         # Only interpolate between start and extinction.
-        mask = ((t >= x.index.min()) & (t <= x.index.max()))
+        mask = ((t >= data.index.min()) & (t <= data.index.max()))
         # Interpolate from previous point.
-        data_ds.loc[k][mask] = x.reindex(t[mask], method='ffill')
+        data_ds.loc[ix][mask] = data.reindex(t[mask], method='ffill')
     data_ds.dropna(axis=0, inplace=True)
     return data_ds
 
@@ -57,10 +67,11 @@ def get_persistence_time(x):
     return t.max() - t.min()
 
 
-def plot_persistence_time(data, chronic=False):
+def plot_persistence_time(chronic=False):
+    filename = get_filename(chronic=chronic)
     # Only plot the first start time.
-    mask = (data.index.get_level_values('start_time') == 0.)
-    data = data[mask]
+    where = 'start_time=0'
+    data = h5.load(filename, where=where)
     not_t_names = [n for n in data.index.names if n != t_name]
     persistence_time = data.groupby(level=not_t_names).apply(
         get_persistence_time)
@@ -68,15 +79,19 @@ def plot_persistence_time(data, chronic=False):
     persistence_time = persistence_time.reset_index(
         name='persistence time (days)')
     # seaborn.factorplot(data=persistence_time,
-    #                    x='start_time', y='persistence time (days)', col='SAT',
-    #                    kind='box', sharey=False)
+    #                    x='persistence time (days)', y='start_time', col='SAT',
+    #                    kind='box', orient='horizontal', sharey=False)
     pyplot.figure()
-    seaborn.violinplot(data=persistence_time,
-                       x='SAT', y='persistence time (days)',
-                       cut=0, linewidth=1)
-    pyplot.xlabel('')
-    locs, labels = pyplot.xticks()
-    pyplot.xticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
+    ax = seaborn.violinplot(data=persistence_time,
+                            x='persistence time (days)', y='SAT',
+                            orient='horizontal', width=0.95,
+                            cut=0, linewidth=1)
+    for col in ax.collections:
+        if isinstance(col, matplotlib.collections.PolyCollection):
+            col.set_linewidth(0)
+    pyplot.ylabel('')
+    locs, labels = pyplot.yticks()
+    pyplot.yticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
     pyplot.tight_layout()
     filename = 'plot_start_times_SATs_persistence_time'
     if chronic:
@@ -90,11 +105,14 @@ def plot_infected_facet(x, color=None, alpha=1, **kwargs):
     x = x.unstack('run')
     t = x.index - x.index.min()
     pyplot.plot(365 * t, x, alpha=alpha, **kwargs)
-    # Use `alpha=1` for the mean, not the value passed in.
-    pyplot.plot(365 * t, x.mean(axis=1), color='black', alpha=1, **kwargs)
+    # `x.fillna(0)` gives mean including those that have gone extinct.
+    # Use `alpha=1` for the mean, not the `alpha` value passed in.
+    pyplot.plot(365 * t, x.fillna(0).mean(axis=1), color='black', alpha=1,
+                **kwargs)
 
 
-def plot_infected(data, chronic=False):
+def plot_infected(chronic=False):
+    data = get_downsampled(chronic=chronic)
     # Only plot the first start time.
     mask = (data.index.get_level_values('start_time') == 0.)
     data = data[mask]
@@ -120,10 +138,11 @@ def get_time_to_peak(x):
     return I.idxmax()[-1] - I.index.get_level_values(t_name).min()
 
 
-def plot_time_to_peak(data, chronic=False):
+def plot_time_to_peak(chronic=False):
+    filename = get_filename(chronic=chronic)
     # Only plot the first start time.
-    mask = (data.index.get_level_values('start_time') == 0.)
-    data = data[mask]
+    where = 'start_time=0'
+    data = h5.load(filename, where=where)
     not_t_names = [n for n in data.index.names if n != t_name]
     time_to_peak = data.groupby(level=not_t_names).apply(
         get_time_to_peak)
@@ -131,16 +150,21 @@ def plot_time_to_peak(data, chronic=False):
     time_to_peak = time_to_peak.reset_index(level=['SAT', 'start_time'],
                                             name='time to peak (days)')
     # seaborn.factorplot(data=time_to_peak,
-    #                    x='start_time', y='time to peak (days)', col='SAT',
+    #                    x='time to peak (days)', y='start_time', col='SAT',
     #                    sharey=False,
-    #                    kind='violin', cut=0, linewidth=1)
+    #                    kind='violin', orient='horizontal',
+    #                    cut=0, linewidth=1)
     pyplot.figure()
-    seaborn.violinplot(data=time_to_peak,
-                       x='SAT', y='time to peak (days)',
-                       cut=0, linewidth=1)
-    pyplot.xlabel('')
-    locs, labels = pyplot.xticks()
-    pyplot.xticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
+    ax = seaborn.violinplot(data=time_to_peak,
+                            x='time to peak (days)', y='SAT',
+                            orient='horizontal', width=0.95,
+                            cut=0, linewidth=1)
+    for col in ax.collections:
+        if isinstance(col, matplotlib.collections.PolyCollection):
+            col.set_linewidth(0)
+    pyplot.ylabel('')
+    locs, labels = pyplot.yticks()
+    pyplot.yticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
     pyplot.tight_layout()
     filename = 'plot_start_times_SATs_time_to_peak'
     if chronic:
@@ -155,26 +179,32 @@ def get_total_infected(x):
     return R.iloc[-1] - R.iloc[0]
 
 
-def plot_total_infected(data, chronic=False):
+def plot_total_infected(chronic=False):
+    filename = get_filename(chronic=chronic)
     # Only plot the first start time.
-    mask = (data.index.get_level_values('start_time') == 0.)
-    data = data[mask]
+    where = 'start_time=0'
+    data = h5.load(filename, where=where)
     not_t_names = [n for n in data.index.names if n != t_name]
     total_infected = data.groupby(level=not_t_names).apply(get_total_infected)
     total_infected.clip_lower(0, inplace=True)
     total_infected = total_infected.reset_index(level=['SAT', 'start_time'],
                                                 name='total infected')
     # seaborn.factorplot(data=total_infected,
-    #                    x='start_time', y='total infected', col='SAT',
+    #                    x='total infected', y='start_time', col='SAT',
     #                    sharey=False,
-    #                    kind='violin', cut=0, linewidth=1)
+    #                    kind='violin', orient='horizontal',
+    #                    cut=0, linewidth=1)
     pyplot.figure()
-    seaborn.violinplot(data=total_infected,
-                       x='SAT', y='total infected',
-                       cut=0, linewidth=1)
-    pyplot.xlabel('')
-    locs, labels = pyplot.xticks()
-    pyplot.xticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
+    ax = seaborn.violinplot(data=total_infected,
+                            x='total infected', y='SAT',
+                            orient='horizontal', width=0.95,
+                            cut=0, linewidth=1)
+    for col in ax.collections:
+        if isinstance(col, matplotlib.collections.PolyCollection):
+            col.set_linewidth(0)
+    pyplot.ylabel('')
+    locs, labels = pyplot.yticks()
+    pyplot.yticks(locs, ['SAT {}'.format(i.get_text()) for i in labels])
     pyplot.tight_layout()
     filename = 'plot_start_times_SATs_total_infected'
     if chronic:
@@ -186,9 +216,8 @@ def plot_total_infected(data, chronic=False):
 if __name__ == '__main__':
     chronic = True
 
-    data, data_ds = get_data(chronic)
-    plot_infected(data_ds, chronic)
-    plot_persistence_time(data, chronic)
-    plot_time_to_peak(data, chronic)
-    plot_total_infected(data, chronic)
+    plot_infected(chronic)
+    # plot_persistence_time(chronic)
+    # plot_time_to_peak(chronic)
+    # plot_total_infected(chronic)
     # pyplot.show()
