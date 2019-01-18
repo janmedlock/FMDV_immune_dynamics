18aca3773111965ca7764135edfd036f97265c30
Jan Medlock <jan.medlock@oregonstate.edu>
2019-01-17 16:30:53 -0800

    Another trivial changes to try to fix bad versions on github.

--- plot_common.py
+++ plot_common.py
@@ -1,1 +1,60 @@
-/annex/objects/SHA256E-s2154--d13ca8e457c572aacaed0fc5b843aa4d841d7f95a6cc1b7ec2ab701bb71b9555.py
+import itertools
+
+import matplotlib.collections
+import numpy
+import pandas
+
+import h5
+
+
+t_name = 'time (y)'
+
+
+def _downsample_group(group, t, not_t_names):
+    # Only keep time index.
+    group = group.reset_index(not_t_names, drop=True)
+    # Only interpolate between start and extinction.
+    mask = ((t >= group.index.min()) & (t <= group.index.max()))
+    # Interpolate from the closest point <= t.
+    return group.reindex(t[mask], method='ffill')
+
+
+def downsample(filename):
+    dt = 1 / 365  # Daily timesteps.
+    store = h5.HDFStore(filename, mode='r')
+    index_names = store.select(columns=[], stop=0).index.names
+    not_t_names = [n for n in index_names if n != t_name]
+    print('Finding t limits.')
+    t_min = numpy.inf
+    t_max = - numpy.inf
+    for chunk in store.select(columns=[], iterator=True):
+        not_t_values = (chunk.index[0][index_names.index(n)]
+                        for n in not_t_names)
+        print(', '.join(f'{k}={v}' for k, v in zip(not_t_names, not_t_values)))
+        t = chunk.index.get_level_values(t_name)
+        t_min = min(t_min, t.min())
+        t_max = max(t_max, t.max())
+    t = numpy.arange(t_min, t_max, dt)
+    data_ds = {}
+    remainder = None
+    # One more empty chunk at the end.
+    for chunk in itertools.chain(store.select(iterator=True), [None]):
+        data = pandas.concat([remainder, chunk], copy=False)
+        grouper = data.groupby(not_t_names)
+        for (i, (ix, group)) in enumerate(grouper):
+            if (chunk is None) or (i < (len(grouper) - 1)):
+                print(', '.join(f'{k}={v}' for k, v in zip(not_t_names, ix)))
+                data_ds[ix] = _downsample_group(group, t, not_t_names)
+            else:
+                # The last group might be continued in the next chunk.
+                remainder = group
+    data_ds = pandas.concat(data_ds, copy=False)
+    data_ds.rename_axis(not_t_names + [t_name], inplace=True, copy=False)
+    data_ds.dropna(axis=0, inplace=True)
+    return data_ds
+
+
+def set_violins_linewidth(ax, lw):
+    for col in ax.collections:
+        if isinstance(col, matplotlib.collections.PolyCollection):
+            col.set_linewidth(0)
--- run_common.py
+++ run_common.py
@@ -1,1 +1,101 @@
-/annex/objects/SHA256E-s3580--dc91e302e246b6affbb4cb4a5d09b6c03390fed2acb7bed149ea90fb40e8dffd.py
+import copy
+import time
+
+from joblib import delayed, Parallel
+import numpy
+import pandas
+
+import herd
+
+
+_SATs = (1, 2, 3)
+
+
+def run_one(run_number, parameters, tmax, *args, **kwargs):
+    '''Run one simulation.'''
+    h = herd.Herd(parameters, run_number=run_number, *args, **kwargs)
+    return h.run(tmax)
+
+
+def run_many(nruns, parameters, tmax, *args, **kwargs):
+    '''Run many simulations in parallel.'''
+    results = Parallel(n_jobs=-1)(
+        delayed(run_one)(i, parameters, tmax, *args, **kwargs)
+        for i in range(nruns))
+    # Make 'run' the outer row index.
+    return pandas.concat(results, keys=range(nruns), names=['run'],
+                         copy=False)
+
+
+def run_SATs(chronic, nruns, tmax, *args, **kwargs):
+    results = {}
+    for SAT in _SATs:
+        p = herd.Parameters(SAT=SAT, chronic=chronic)
+        print('Running SAT {}.'.format(SAT))
+        t0 = time.time()
+        results[SAT] = run_many(nruns, p, tmax, *args, **kwargs)
+        t1 = time.time()
+        print('Run time: {} seconds.'.format(t1 - t0))
+    return pandas.concat(results, names=['SAT'], copy=False)
+
+
+def run_start_times(nruns, SAT, chronic, tmax, logging_prefix='',
+                    *args, **kwargs):
+    results = {}
+    # Every month.
+    for start_time in numpy.arange(0, 1, 1 / 12):
+        p = herd.Parameters(SAT=SAT, chronic=chronic)
+        p.start_time = start_time
+        logging_prefix_ = (logging_prefix
+                           + 'Start time {:g} / 12'.format(start_time * 12))
+        print('Running {}.'.format(logging_prefix_))
+        logging_prefix_ += ', '
+        t0 = time.time()
+        results[start_time] = run_many(nruns, p, tmax,
+                                       logging_prefix=logging_prefix_,
+                                       *args, **kwargs)
+        t1 = time.time()
+        print('Run time: {} seconds.'.format(t1 - t0))
+    return pandas.concat(results, names=['start_time'], copy=False)
+
+
+def run_start_times_SATs(nruns, chronic, tmax, *args, **kwargs):
+    results = {}
+    for SAT in _SATs:
+        results[SAT] = run_start_times(nruns, SAT, chronic, tmax,
+                                       logging_prefix='SAT {}, '.format(SAT),
+                                       *args, **kwargs)
+    return pandas.concat(results, names=['SAT'], copy=False)
+
+
+def _run_sample(run_number, parameters, sample, tmax, *args, **kwargs):
+    '''Run one simulation.'''
+    p = copy.copy(parameters)
+    for k, v in sample.items():
+        setattr(p, k, v)
+    h = herd.Herd(p, run_number=run_number, *args, **kwargs)
+    return h.run(tmax)
+
+
+def _run_samples_SAT(SAT, chronic, tmax, *args, **kwargs):
+    '''Run many simulations in parallel.'''
+    from herd.samples import samples  # There's some cost to importing this...
+    parameters = herd.Parameters(SAT=SAT, chronic=chronic)
+    print('Running SAT {}.'.format(SAT))
+    t0 = time.time()
+    results = Parallel(n_jobs=-1)(
+        delayed(_run_sample)(i, parameters, s, tmax, *args, **kwargs)
+        for i, s in samples[SAT].iterrows())
+    t1 = time.time()
+    print('Run time: {} seconds.'.format(t1 - t0))
+    return pandas.concat(results, keys=range(len(samples)),
+                         names=['sample'], copy=False)
+
+
+def run_samples(chronic, tmax, *args, **kwargs):
+    results = {}
+    for SAT in (1, 2, 3):
+        results[SAT] = _run_samples_SAT(SAT, chronic, tmax,
+                                        logging_prefix='SAT {}, '.format(SAT),
+                                        *args, **kwargs)
+    return pandas.concat(results, names=['SAT'], copy=False)
