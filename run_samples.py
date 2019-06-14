813f9e040cd4d328df05581fe250f0b5f532e194
Jan Medlock <jan.medlock@oregonstate.edu>
2019-06-14 10:30:57 -0700

    Remove subsampling. I might have sped things up enough...

--- run_samples.py
+++ run_samples.py
@@ -32,34 +32,31 @@ def _run_sample(parameters, sample, tmax, path, sample_number, logging_prefix):
         numpy.save(filename, df.to_records())
 
 
-def _run_samples_SAT(model, SAT, tmax, n_subsamples, seed, path,
-                     logging_prefix):
+def _run_samples_SAT(model, SAT, tmax, path, logging_prefix):
     '''Run many simulations in parallel.'''
     path_SAT = os.path.join(path, str(SAT))
     os.makedirs(path_SAT, exist_ok=True)
     logging_prefix_SAT = logging_prefix + f'SAT {SAT}, '
     parameters = herd.Parameters(model=model, SAT=SAT)
     samples = herd.samples.load(model=model, SAT=SAT)
-    subsamples = samples.sample(n_subsamples, random_state=seed).sort_index()
     return (delayed(_run_sample)(parameters, s, tmax, path_SAT, i,
                                  logging_prefix_SAT)
-            for (i, s) in subsamples.iterrows())
+            for (i, s) in samples.iterrows())
 
 
-def _run_samples_model(model, tmax, n_subsamples, seed, path):
+def _run_samples_model(model, tmax, path):
     path_model = os.path.join(path, model)
     os.makedirs(path_model, exist_ok=True)
     logging_prefix = f'model {model}, '
     return itertools.chain.from_iterable(
-        _run_samples_SAT(model, SAT, tmax, n_subsamples, seed, path_model,
-                         logging_prefix)
+        _run_samples_SAT(model, SAT, tmax, path_model, logging_prefix)
         for SAT in run_common._SATs)
 
 
-def run_samples(tmax, n_subsamples, seed):
+def run_samples(tmax):
     os.makedirs(_path, exist_ok=True)
     jobs = itertools.chain.from_iterable(
-        _run_samples_model(model, tmax, n_subsamples, seed, _path)
+        _run_samples_model(model, tmax, _path)
         for model in run_common._models)
     Parallel(n_jobs=-1)(jobs)
 
@@ -96,8 +93,6 @@ def combine():
 
 if __name__ == '__main__':
     tmax = 10
-    n_subsamples = 2000
-    seed = 1
 
-    run_samples(tmax, n_subsamples, seed)
+    run_samples(tmax)
     # combine()
