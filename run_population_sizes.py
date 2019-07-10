3fe348c446412e1fc34cdb92adcc965cee023497
Jan Medlock <jan.medlock@oregonstate.edu>
2019-07-10 10:30:17 -0700

    Add append_run_SATs().

--- run_population_sizes.py
+++ run_population_sizes.py
@@ -23,10 +23,24 @@ def run_population_size(model, SAT, population_size, tmax, nruns, hdfstore):
     hdfstore.put(df, min_itemsize=run_common._min_itemsize)
 
 
+def append_run_SATs(store_out, nruns):
+    '''Append the data from 'run_SATs.h5' to `store_out`.'''
+    p = herd.Parameters()
+    where = f'run<{nruns}'
+    with h5.HDFStore('run_SATs.h5', mode='r') as store_in:
+        for chunk in store_in.select(where=where, iterator=True):
+            # Add 'population_size' level to the index.
+            run_common._insert_index_levels(chunk, 2,
+                                            population_size=p.population_size)
+            store_out.put(chunk, min_itemsize=run_common._min_itemsize)
+
+
 if __name__ == '__main__':
     population_sizes = (
         # tuple(range(100, 1000, 100)) +
-        tuple(range(1000, 3000, 1000))
+        # population_size = 1000 is in 'run_SATs.h5'.
+        # `append_run_SATs()` will copy these into 'run_population_sizes.h5'.
+        (2000, )
     )
     nruns = 1000
     tmax = 10
@@ -39,4 +53,5 @@ if __name__ == '__main__':
                 for SAT in (1, 2, 3):
                     run_population_size(model, SAT, population_size,
                                         tmax, nruns, store)
+        # append_run_SATs(store, nruns)
         store.repack()
