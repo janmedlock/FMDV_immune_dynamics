ddc0a0a45e217b3e94a1ce813ac33c835c7dde16
Jan Medlock <jan.medlock@oregonstate.edu>
2019-07-27 12:15:17 -0700

    Whitespace tweak.

--- herd/immunity_waning.py
+++ herd/immunity_waning.py
@@ -8,7 +8,7 @@ class gen(RV):
     recovered to susceptible.'''
     def __init__(self, parameters, *args, **kwargs):
         self.immunity_waning_duration = parameters.immunity_waning_duration
-        distn = expon(scale = self.immunity_waning_duration)
+        distn = expon(scale=self.immunity_waning_duration)
         super()._copyattrs(distn)
 
     def __repr__(self):
