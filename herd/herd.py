49a8b72d35417b1b728693803f075ef843a8a652
Jan Medlock <jan.medlock@oregonstate.edu>
2019-07-27 12:15:35 -0700

    Add partial immunity state.

--- herd/herd.py
+++ herd/herd.py
@@ -10,7 +10,7 @@ from herd.random_variables import RandomVariables
 
 
 statuses = ('maternal immunity', 'susceptible', 'exposed',
-            'infectious', 'chronic', 'recovered')
+            'infectious', 'chronic', 'recovered', 'partial immunity')
 
 
 class Herd(set):
