cbc798a8623d7d259ed9d7175e334103e6b4bdaf
Jan Medlock <jan.medlock@oregonstate.edu>
2020-01-22 16:32:09 -0800

    Clean ups from immune_dynamics_test branch.

--- test/initial_conditions.py
+++ test/initial_conditions.py
@@ -63,6 +63,7 @@ class Solver:
         The result is a vector over ages a.
         '''
         T = sparse.lil_matrix((self.I, self.K))
+        # i = 0.
         # T[0, self.get_k(0, 0)] = 0  # No op.
         for i in range(1, self.I):
             T[i, self.get_k(i, [0, i])] = self.age_step / 2
@@ -76,20 +77,19 @@ class Solver:
                                 2 - rate_out_times_age_step  ],
             [0, -1])
 
-    @classmethod
-    def get_A_YY(cls, rate_out_times_age_step):
-        I = len(rate_out_times_age_step) + 1
-        K = cls.get_k(I, 0)
-        A = sparse.lil_matrix((K, K))
-        k = cls.get_k(0, 0)
+    def get_A_YY(self, rate_out_times_age_step):
+        A = sparse.lil_matrix((self.K, self.K))
+        # i = 0.
+        k = self.get_k(0, 0)
         A[k, k] = 1
-        for i in range(1, I):
-            k = cls.get_k(i, 0)
+        for i in range(1, self.I):
+            k = self.get_k(i, 0)
             A[k, k] = - 2
-            for (j, r) in enumerate(rate_out_times_age_step[:i], 1):
-                # k = cls.get_k(i, j)
-                # k_1 = cls.get_k(i - 1, j - 1)
-                [k, k_1] = cls.get_k([i, i - 1], [j, j - 1])
+            for j in range(1, i + 1):
+                # k = self.get_k(i, j)
+                # k_1 = self.get_k(i - 1, j - 1)
+                [k, k_1] = self.get_k([i, i - 1], [j, j - 1])
+                r = rate_out_times_age_step[j - 1]
                 A[k, [k, k_1]] = [- 2 - r, 2 - r]
         return A
 
@@ -177,10 +177,12 @@ class Solver:
                             * numpy.ones_like(self.ages_mid))
         A_ES = sparse.lil_matrix((self.K, self.I))
         A_EL = sparse.lil_matrix((self.K, self.I))
+        # i = 0.
         # A_ES[k, self.get_k(0, 0)] = 0  # No op.
         # A_EL[k, self.get_k(0, 0)] = 0  # No op.
-        for (i, h) in enumerate(hazard_infection, 1):
+        for i in range(1, self.I):
             k = self.get_k(i, 0)
+            h = hazard_infection[i - 1]
             A_ES[k, [i, i - 1]] = h
             A_EL[k, [i, i - 1]] = h
         with numpy.errstate(divide='ignore'):
@@ -358,10 +360,10 @@ if __name__ == '__main__':
     # parameters.maternal_immunity_duration_mean = numpy.inf
     # No S -> E.
     # Turning on infection gives an incorrect blip around a = 0.5.
-    hazard_infection = 0
+    # hazard_infection = 0
     # No E -> I.
     # Turning on progression gives nonsense.
-    # parameters.progression_mean = numpy.inf
+    parameters.progression_mean = numpy.inf
     # No I -> C.
     # parameters.probability_chronic = 0
     # No I -> R (and C).
@@ -373,7 +375,7 @@ if __name__ == '__main__':
     #     parameters.antibody_loss_hazard_beta = 0
     # No L -> R.
     # parameters.antibody_gain_hazard = 0
-    RVs = RandomVariables(parameters)
+    RVs = RandomVariables(parameters, _initial_conditions=False)
     age_max = 10
     age_step = 0.1
     solver = Solver(hazard_infection, RVs, age_max, age_step)
