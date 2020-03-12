f2307444c11f8cd2965396e1a72a9a5ffa8c047e
Jan Medlock <jan.medlock@oregonstate.edu>
2020-03-11 17:17:14 -0700

    Changed antibody_gain.sf() so that the first argument is time since time0, not absolute time, i.e. change time - time0 -> time. .isf(), .rvs() etc. were also changed to match.
    Fixed an error in antibody_gain._support() that had the cases
    alpha <> beta backwards.

--- herd/antibody_gain.py
+++ herd/antibody_gain.py
@@ -27,26 +27,27 @@ class gen(RV):
     def _support(self):
         '''Find the interval where the hazard is positive.'''
         if self.alpha == self.beta:
-            # If alpha == beta == 0, the hazard is 0 everywhere.
-            # The methods that use _support() all continue to give
-            # correct results with any return value here.
+            # The support is (-inf, inf) unless
+            # alpha == beta == 0, i.e. the hazard is 0 everywhere,
+            # but the methods that use _support() all continue to give
+            # correct results in that case with any return value here.
             return (-numpy.inf, numpy.inf)
         else:
             # Where the linear part of the hazard would cross 0.
             time_intercept = self.time_min - self.alpha / self._slope
             if self.alpha > self.beta:
-                return (time_intercept, numpy.inf)
-            else:
                 return (-numpy.inf, time_intercept)
+            else:
+                return (time_intercept, numpy.inf)
 
     def logsf(self, time, time0):
         '''Logarithm of the survival function.'''
         support = self._support()
-        time = numpy.clip(time, *support)
-        time0 = numpy.clip(time0, *support)
-        time_mid = (time + time0) / 2
-        return numpy.where(time >= time0,
-                           - self.hazard(time_mid) * (time - time0),
+        u_0 = numpy.clip(time0, *support)
+        u = numpy.clip(time0 + time, *support)
+        u_mid = (u + u_0) / 2
+        return numpy.where(u >= u_0,
+                           - self.hazard(u_mid) * (u - u_0),
                            0)
 
     def sf(self, time, time0):
@@ -55,24 +56,44 @@ class gen(RV):
 
     def isf(self, q, time0):
         '''Inverse survival function.'''
-        # log q = - \int_{time_0}^{time} slope * (u - time_min + alpha) du
-        # Let t   = time   - time_min,
-        #     t_0 = time_0 - time_min.
-        # Then
-        # 0 = slope / 2 * t ** 2
-        #     + alpha * t
-        #     + log q - slope / 2 * t_0 ** 2 - alpha * t_0
-        # Coefficients of the quadratic polynomial we need to solve.
-        t0 = time0 - self.time_min
-        a = self._slope / 2
-        b = self.alpha
-        c = (numpy.ma.log(q)
-             - self._slope / 2 * t0 ** 2
-             - self.alpha * t0)
-        t = numpy.ma.filled(
-            (- b + numpy.ma.sqrt(b ** 2 - 4 * a * c)) / 2 / a,
-            numpy.inf)
-        return t + self.time_min
+        support = self._support()
+        if (support[0] <= time0 <= support[1]):
+            # log q = - \int_{time0}^{time0 + time}
+            #                slope * (u - time_min) + alpha du.
+            # Let t0 = time0 - time_min,
+            #     t = time0 + time - time_min.
+            # Then
+            # log q = - \int_{t0}^t slope * v + alpha dv,
+            # or
+            # 0 = slope / 2 * t ** 2
+            #     + alpha * t
+            #     + log q - slope / 2 * t0 ** 2 - alpha * t0
+            # Coefficients of the quadratic polynomial we need to solve.
+            t0 = time0 - self.time_min
+            a = self._slope / 2
+            b = self.alpha
+            c = (numpy.ma.log(q)
+                 - self._slope / 2 * t0 ** 2
+                 - self.alpha * t0)
+            # The sqrt is imaginary when the q is very small
+            # because there is some chance that an animal survives until
+            # the hazard hits 0. We will say that the survival time
+            # is infinity in this case.
+            sqrt_D = numpy.ma.sqrt(b ** 2 - 4 * a * c)
+            t = numpy.ma.filled(
+                numpy.ma.stack(((- b + sqrt_D) / 2 / a,
+                                (- b - sqrt_D) / 2 / a)),
+                numpy.inf).min(axis=0)
+            # t -> time_min + t
+            # undoes the change of variables in the integral
+            # u -> u - time_min.
+            # time -> time - time0
+            # gives a result that is time since time0,
+            # as the other RVs do.
+            time = self.time_min + t - time0
+        else:
+            time = numpy.inf * numpy.ones_like(q)
+        return time
 
     def cdf(self, time, time0):
         '''Cumulative distribution function.'''
@@ -84,7 +105,8 @@ class gen(RV):
 
     def logpdf(self, time, time0):
         '''Logarithm of the probability density function.'''
-        return numpy.log(self.hazard(time)) + self.logsf(time, time0)
+        return (numpy.log(self.hazard(time0 + time))
+                + self.logsf(time, time0))
 
     def pdf(self, time, time0):
         '''Probability density function.'''
