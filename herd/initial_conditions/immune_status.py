2f635ccc8b82589884f97e8f9378f0bedd485695
Jan Medlock <jan.medlock@oregonstate.edu>
2020-04-04 11:17:30 -0700

    Some components of the solution are very close to 0 but negative. Clip those to be 0.

--- herd/initial_conditions/immune_status.py
+++ herd/initial_conditions/immune_status.py
@@ -377,6 +377,7 @@ class Solver:
         P.sort_index(axis='columns', inplace=True)
         assert ((P >= 0) | numpy.isclose(P, 0)).all(axis=None)
         assert numpy.isclose(P.sum(axis='columns'), 1).all()
+        P.clip(lower=0, inplace=True)
         return P
 
     def get_hazard_infection(self, P):
