2c65c2f1fcd2c0f66e87e353deada29b76b28288
Jan Medlock <jan.medlock@oregonstate.edu>
2020-10-30 18:52:07 -0700

    Update initial guess for fixed_point solver.

--- herd/initial_conditions/immune_status.py
+++ herd/initial_conditions/immune_status.py
@@ -490,7 +490,7 @@ class Solver:
                                self.get_newborn_proportion_immune(P)))
 
     def solve(self):
-        x_guess = (6, 0.8)
+        x_guess = (20, 0.9)
         y_sol = optimize.fixed_point(self.solve_objective,
                                      self.transform(x_guess))
         P = self.solve_step(y_sol)
