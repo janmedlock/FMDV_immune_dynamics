d42370b16c2c20c6bf686f35cd778db286d453a2
Jan Medlock <jan.medlock@oregonstate.edu>
2022-05-25 12:41:25 -0700

    Use herd.initial_conditions.immune_status.Solver.get_A().

--- test/pdfs_finite.py
+++ test/pdfs_finite.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 import copy
-import os.path
 import sys
 
-import numpy
+import pandas
 
 sys.path.append('..')
 import herd
 import herd.chronic_recovery
+import herd.initial_conditions.immune_status
 import herd.progression
 import herd.recovery
 import herd.samples
@@ -22,16 +22,25 @@ RVS = {
 }
 
 
-def test_sample(parameters, sample, sat, n):
-    p = copy.copy(parameters)
-    for (k, v) in sample.items():
-        setattr(p, k, v)
+def test_sample(parameters, sample, sat, idx):
+    params = copy.copy(parameters)
+    for (key, val) in sample.items():
+        setattr(params, key, val)
+    shapes = pandas.Series(dtype=float)
     for rv in RVS:
-        pdf_0 = getattr(herd, rv).gen(p).pdf(0)
-        if numpy.isinf(pdf_0):
-            shape = getattr(p, f'{rv}_shape')
-            print(f'{sat=}, {n=}, {rv=}, {shape=}')
-            assert shape < 1
+        name = f'{rv}_shape'
+        shapes[name] = getattr(params, name)
+    is_small = (shapes < 1)
+    solver = herd.initial_conditions.immune_status.Solver(params)
+    try:
+        solver.get_A()
+    except AssertionError:
+        shapes_small = ', '.join(f'{key}={val}'
+                                 for (key, val) in shapes[is_small].items())
+        print(f'{sat=}, {idx=}, {shapes_small}')
+        assert is_small.any()
+    else:
+        assert ~(is_small.any())
 
 
 def test_sat(sat):
