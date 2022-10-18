b90ee866c3ab1baef33ce35c232d854e37369791
Jan Medlock <jan.medlock@oregonstate.edu>
2022-10-18 13:02:29 -0500

    Add scripts to run and plot sensitivity to varying population size.

---
+++ population_size_run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Run simulations with varying population size. This produces a file
+called `population_size.h5`.'''
+
+import common
+import h5
+import population_size
+
+
+if __name__ == '__main__':
+    NRUNS = 1000
+
+    with h5.HDFStore(population_size.store_path) as store:
+        for SAT in common.SATs:
+            for popsize in population_size.population_sizes:
+                population_size.run(SAT, popsize, NRUNS, store)
+        store.repack()
