513a940ff14c4ed90f65d30c0fdb3a39dc011831
Jan Medlock <medlockj@oregonstate.edu>
2025-09-16 16:35:57 -0700

    Only plot age density and status probability. Tweak style.

--- initial_conditions_plot.py
+++ initial_conditions_plot.py
@@ -17,7 +17,7 @@ import supplemental_materials
 
 
 rc = common.rc | supplemental_materials.rc | common.rc_text_small | {
-    'figure.figsize': (supplemental_materials.WIDTH_MAXIMUM, 6),
+    'figure.figsize': (supplemental_materials.WIDTH_MAXIMUM, 4.5),
     'axes.spines.top': False,
 }
 
@@ -25,57 +25,77 @@ rc = common.rc | supplemental_materials.rc | common.rc_text_small | {
 def plot_age_density(ICs, ages, ax):
     '''Plot the density of the stable age distribution.'''
     p = ICs.ages.pdf(ages)
-    ax.plot(ages, p, color='black', linewidth=1, zorder=3)
+    ax.plot(ages, p,
+            color='black', linewidth=1, zorder=3)
     if ax.get_subplotspec().is_first_col():
         ax.set_ylabel('Age density (year$^{-1}$)')
         ax.set_ylim(bottom=0)
         ax.yaxis.set_major_locator(
+            matplotlib.ticker.MultipleLocator(0.2)
+        )
+        ax.yaxis.set_minor_locator(
             matplotlib.ticker.MultipleLocator(0.1)
         )
 
 
-def plot_status_probability_unconditional(ICs, ages, ax):
-    '''Plot probability of being in each status *not* conditioned on
-    being alive vs. age.'''
+def plot_status_probability(ICs, ages, ax):
+    '''Plot probability of being in each status vs. age.'''
     p = ICs.immune_status_pdf(ages)
-    ax.stackplot(ages, p.T, labels=p.columns.str.capitalize(), zorder=3)
+    ax.stackplot(ages, p.T,
+                 labels=p.columns.str.capitalize(),
+                 zorder=3)
     if ax.get_subplotspec().is_first_col():
-        ax.set_ylabel('Status probability')
+        ax.set_ylabel('Status\nprobability')
         ax.set_ylim(0, 1)
         ax.yaxis.set_major_locator(
-            matplotlib.ticker.MultipleLocator(0.2)
+            matplotlib.ticker.MultipleLocator(0.5)
+        )
+        ax.yaxis.set_minor_locator(
+            matplotlib.ticker.MultipleLocator(0.1)
         )
 
 
-def plot_status_probability(ICs, ages, ax):
+def plot_status_probability_given_alive(ICs, ages, ax):
     '''Plot the probability of being in each status conditioned on
     being alive vs. age.'''
     p = ICs.immune_status_conditional_pdf(ages)
-    ax.stackplot(ages, p.T, labels=p.columns.str.capitalize(), zorder=3)
+    ax.stackplot(ages, p.T,
+                 labels=p.columns.str.capitalize(),
+                 alpha=0.8, zorder=3)
     if ax.get_subplotspec().is_first_col():
+        # ax.set_ylabel('Status conditional\nprobability')
         ax.set_ylabel('Status probability')
         ax.set_ylim(0, 1)
         ax.yaxis.set_major_locator(
-            matplotlib.ticker.MultipleLocator(0.2)
+            matplotlib.ticker.MultipleLocator(0.5)
+        )
+        ax.yaxis.set_minor_locator(
+            matplotlib.ticker.MultipleLocator(0.1)
         )
 
 
 def plot_joint_density(ICs, ages, ax):
     '''Plot the joint density.'''
     p = ICs.pdf(ages)
-    ax.stackplot(ages, p.T, labels=p.columns.str.capitalize(), zorder=3)
+    ax.stackplot(ages, p.T,
+                 labels=p.columns.str.capitalize(),
+                 zorder=3)
     if ax.get_subplotspec().is_first_col():
-        ax.set_ylabel('Joint density (year$^{-1}$)')
+        ax.set_ylabel('Joint density\n(year$^{-1}$)')
         ax.set_ylim(bottom=0)
         ax.yaxis.set_major_locator(
+            matplotlib.ticker.MultipleLocator(0.2)
+        )
+        ax.yaxis.set_minor_locator(
             matplotlib.ticker.MultipleLocator(0.1)
         )
 
 
 plot_fcns = (
     plot_age_density,
-    plot_status_probability,
-    plot_joint_density,
+    # plot_status_probability,
+    plot_status_probability_given_alive,
+    # plot_joint_density,
 )
 
 
@@ -105,11 +125,7 @@ def plot_SAT(axes, SAT, ages):
             ax.xaxis.set_tick_params(which='both',
                                      labelleft=False, labelright=False)
             ax.xaxis.offsetText.set_visible(False)
-        if subplotspec.is_first_col():
-            ax.yaxis.set_minor_locator(
-                matplotlib.ticker.AutoMinorLocator(2)
-            )
-        else:
+        if not subplotspec.is_first_col():
             ax.yaxis.set_tick_params(which='both',
                                      labelleft=False, labelright=False)
             ax.yaxis.offsetText.set_visible(False)
@@ -124,7 +140,7 @@ def plot_SATs(save=True):
         # Add an extra row for the legend.
         gs = fig.add_gridspec(
             nrows + 1, ncols,
-            height_ratios=((1,) * nrows + (0.3,)),
+            height_ratios=((1,) * nrows + (0.35,)),
         )
         axes = numpy.empty((nrows, ncols), dtype=object)
         axes[0, 0] = None  # `sharex` & `sharey` for `axes[0, 0]`.
