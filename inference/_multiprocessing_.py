a620812200fb5fc081ea2baa652788359e85ec7c
Jan Medlock <jan.medlock@oregonstate.edu>
2026-01-30 10:42:10 -0800

    Improve documentation.

--- inference/_multiprocessing_.py
+++ inference/_multiprocessing_.py
@@ -1,9 +1,12 @@
-'''Utilities for `multiprocessing.Pool()`.'''
+'''`multiprocessing.Pool()` for nested functions.'''
 
 import multiprocessing
 
 
 class _PoolProxy:
+    '''Context manager to use an existing `multiprocessing.Pool()`,
+    without calling `multiprocessing.Pool.__exit__()` on exit.'''
+
     def __init__(self, pool, **kwds):
         assert len(kwds) == 0
         self.pool = pool
@@ -12,14 +15,19 @@ class _PoolProxy:
         return self.pool
 
     def __exit__(self, *args):
-        pass
+        '''Exit without calling `self.pool.close()`.'''
 
     def __getattr__(self, attr):
+        '''Get `attr` from `self.pool`.'''
         return getattr(self.pool, attr)
 
 
-def Pool(pool=None, **kwds):
-    '''Make a new `multiprocessing.Pool()` or proxy an existing one.'''
+def Pool(pool=None, **kwds):  # pylint: disable=invalid-name
+    '''Factory to a new `multiprocessing.Pool()` or proxy an existing
+    one. It is intended for nested functions that use a shared
+    `multiprocessing.Pool()` with multiple entry points, so the
+    `multiprocessing.Pool()` can be created in the first function
+    called and passed to subsequent functions.'''
     if pool is None:
         return multiprocessing.Pool(**kwds)
     return _PoolProxy(pool, **kwds)
