8c9f60002937390ca8b2ddafaeaa59f413ba0931
Jan Medlock <jan.medlock@oregonstate.edu>
2026-01-30 02:56:36 -0800

    Cleanup.

--- inference/_multiprocessing_.py
+++ inference/_multiprocessing_.py
@@ -1,14 +1,25 @@
 '''Utilities for `multiprocessing.Pool()`.'''
 
-import contextlib
 import multiprocessing
+
+
+class _PoolProxy:
+    def __init__(self, pool, **kwds):
+        assert len(kwds) == 0
+        self.pool = pool
+
+    def __enter__(self):
+        return self.pool
+
+    def __exit__(self, *args):
+        pass
+
+    def __getattr__(self, attr):
+        return getattr(self.pool, attr)
 
 
 def Pool(pool=None, **kwds):
     '''Make a new `multiprocessing.Pool()` or proxy an existing one.'''
     if pool is None:
         return multiprocessing.Pool(**kwds)
-    # Return a context manager that returns `pool` on enter and does
-    # nothing on exit.
-    assert len(kwds) == 0
-    return contextlib.nullcontext(pool)
+    return _PoolProxy(pool, **kwds)
