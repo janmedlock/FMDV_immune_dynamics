a1bdc299cd9780b4680508ab78b54cc314f5485a
Jan Medlock <jan.medlock@oregonstate.edu>
2026-01-30 02:05:12 -0800

    Add a description of nullcontext().

--- inference/_multiprocessing_.py
+++ inference/_multiprocessing_.py
@@ -8,5 +8,7 @@ def Pool(pool=None, **kwds):
     '''Make a new `multiprocessing.Pool()` or proxy an existing one.'''
     if pool is None:
         return multiprocessing.Pool(**kwds)
+    # Return a context manager that returns `pool` on enter and does
+    # nothing on exit.
     assert len(kwds) == 0
     return contextlib.nullcontext(pool)
