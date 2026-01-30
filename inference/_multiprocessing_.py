83faa2423cc96eaf1ce85b8b078c1eded719fb52
Jan Medlock <jan.medlock@oregonstate.edu>
2026-01-30 01:52:08 -0800

    Simplify.

--- inference/_multiprocessing_.py
+++ inference/_multiprocessing_.py
@@ -4,17 +4,9 @@ import contextlib
 import multiprocessing
 
 
-class Pool:
+def Pool(pool=None, **kwds):
     '''Make a new `multiprocessing.Pool()` or proxy an existing one.'''
-    def __init__(self, pool=None, **kwds):
-        if pool is None:
-            self.pool = multiprocessing.Pool(**kwds)
-        else:
-            assert len(kwds) == 0
-            self.pool = contextlib.nullcontext(pool)
-
-    def __enter__(self):
-        return self.pool.__enter__()
-
-    def __exit__(self, *args):
-        return self.pool.__exit__(*args)
+    if pool is None:
+        return multiprocessing.Pool(**kwds)
+    assert len(kwds) == 0
+    return contextlib.nullcontext(pool)
