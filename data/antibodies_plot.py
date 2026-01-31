c93b8b1a88ed4122e826dbea2c67db827e219611
Jan Medlock <jan.medlock@oregonstate.edu>
2026-01-30 16:42:37 -0800

    Add start at script to plot antibody distribution.

---
+++ data/antibodies_plot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''Plot the distribution of antibodies.'''
+
+import pathlib
+
+import matplotlib.pyplot
+import seaborn
+
+from context import plotting
+import data_
+
+
+ALPHA = 0.7
+
+
+rc = plotting.rc | plotting.rc_text_small | {
+    'figure.figsize': (plotting.WIDTH_MAXIMUM['double_column'], 4.5),
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'axes.grid.axis': 'y',
+}
+
+
+def plot_antibodies(antibodies_, save=True, show=True):
+    '''Plot the antibody titers by SAT.'''
+    with seaborn.axes_style('whitegrid'), matplotlib.pyplot.rc_context(rc=rc):
+        (fig, ax) = matplotlib.pyplot.subplots()
+        sats = antibodies_.SAT.unique()
+        palette = {sat: plotting.SAT_COLORS[sat] for sat in sats}
+        assert len(ax.collections) == 0
+        seaborn.violinplot(
+            antibodies_, x='SAT', y='titer', hue='SAT',
+            palette=palette, alpha=ALPHA, saturation=1,
+            cut=0,
+            inner='box', inner_kws={'whis_width': 0, 'solid_capstyle': 'butt'},
+            legend=False, ax=ax,
+        )
+        for collection in ax.collections:
+            collection.set_linewidth(0)
+        ax.set_xticks(ax.get_xticks(), [f'SAT{sat}' for sat in sats])
+        ax.xaxis.label.set_visible(False)
+        ax.set_ylabel(r'log$_{10}$ antibody titer')
+        ax.axhline(
+            data_.ANTIBODY_TITER_CUTOFF,
+            linestyle='dotted', color='black', zorder=0.9,
+        )
+        if save:
+            path_file = pathlib.Path(__file__)
+            path = path_file.with_stem(path_file.stem.replace('_plot', ''))
+            for suffix in ('.pdf', '.png'):
+                fig.savefig(path.with_suffix(suffix))
+        if show:
+            matplotlib.pyplot.show()
+
+
+if __name__ == '__main__':
+    antibodies = data_.load()
+    plot_antibodies(antibodies, save=False)
