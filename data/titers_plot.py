3d4d9a7fd7f8e98a7ab74a3f92ba06247296bf96
Jan Medlock <jan.medlock@oregonstate.edu>
2026-02-03 16:07:54 -0800

    Make plots of titer timeseries by SAT and animal.

---
+++ data/titers_plot.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+'''Plot titer time-series by SAT and animal.'''
+
+import functools
+import math
+import pathlib
+
+import matplotlib.dates
+import matplotlib.pyplot
+import pandas
+import seaborn
+
+from context import plotting
+import data_
+
+
+_CONSEC_OBS_MIN = 6
+
+_TICK_LABELSIZE = 6
+_TICK_MINOR_NDIVS = 2
+rc = (
+    plotting.rc
+    | plotting.SupplementalMaterials.rc
+    | plotting.rc_text_small
+    | {
+        'figure.figsize': (plotting.SupplementalMaterials.WIDTH_MAXIMUM, 6),
+        'font.size': 5,
+        'xtick.labelsize': _TICK_LABELSIZE,
+        'ytick.labelsize': _TICK_LABELSIZE,
+        'axes.labelsize': 7,
+        'axes.titlesize': 8,
+        'legend.fontsize': 7,
+        'xtick.minor.ndivs': 2,
+        'xtick.minor.visible': True,
+        'ytick.minor.ndivs': 2,
+        'ytick.minor.visible': True,
+        'axes.titlepad': 0,
+        'lines.linewidth': 0.5,
+    }
+)
+
+
+@functools.lru_cache(maxsize=1)
+def load():
+    '''Load the data.'''
+    observations = data_.load_observations()
+    keep = observations.consecutive_observations >= _CONSEC_OBS_MIN
+    keep_ids = observations[keep].index
+    data = data_.load()
+    return data[data['ID'].isin(keep_ids)]
+
+
+_COL_WRAP = 7
+_SOURCE_PATH = pathlib.Path(__file__)
+_OUTPUT_PATH_STEM_BASE = _SOURCE_PATH.with_stem(
+    _SOURCE_PATH.stem.removesuffix('_plot')
+)
+
+
+def plot_titers(data, save=True, show=False):
+    '''Plot the titers.'''
+
+    def set_ylabel(fg):
+        fg.set_ylabels('')
+        # Find the axis in the middle row, first column.
+        nrows = math.ceil(len(fg.axes) / _COL_WRAP)
+        row = nrows // 2
+        ax = fg.facet_axis(0, row * _COL_WRAP)
+        ax.set_ylabel(r'log$_{10}$ antibody titer')
+
+    grouper = data.groupby('SAT')
+    # Avoid a warning by disabling constrained layout here
+    # and re-enabling later.
+    rc_ = rc | {'figure.constrained_layout.use': False}
+    with matplotlib.pyplot.rc_context(rc=rc_):
+        for (sat, group) in grouper:
+            fg = seaborn.FacetGrid(group,
+                                   col='ID', col_wrap=_COL_WRAP,
+                                   ylim=(1, 3))
+            fg.figure.set_layout_engine('constrained')
+            fg.figure.set_size_inches(*rc['figure.figsize'])
+            fg.map(matplotlib.pyplot.plot, 'date', 'titer',
+                   color='black', marker='o', markersize=1)
+            fg.refline(y=data_.ANTIBODY_TITER_CUTOFF,
+                       color='0.3', linestyle='dotted', zorder=1.5)
+            fg.set_xlabels('')
+            set_ylabel(fg)
+            fg.set_titles('{col_var} {col_name}')
+            fg.set(xlim=(pandas.Timestamp('2014-01-01'), None))
+            for ax in fg.axes:
+                ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+            if save:
+                output_path_stem = _OUTPUT_PATH_STEM_BASE.with_stem(
+                    _OUTPUT_PATH_STEM_BASE.stem + f'_sat{sat}'
+                )
+                output_path = output_path_stem.with_suffix('.pdf')
+                plotting.savefig(fg.figure, output_path)
+        if show:
+            matplotlib.pyplot.show()
+
+
+if __name__ == '__main__':
+    data__ = load()
+    plot_titers(data__, save=False)
